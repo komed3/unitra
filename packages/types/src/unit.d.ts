a136c5b297d80f7d7826aa272f5290c266810f39
komed3 <pk97@outlook.de>
2026-05-28 17:57:00 +0200

    add unit def

--- packages/types/src/unit.d.ts
+++ packages/types/src/unit.d.ts
@@ -10,3 +10,17 @@ export type UnitRef<
   readonly __type: T;
   readonly __dim: D;
 };
+
+export type UnitDef<
+  D extends Dimension = Dimension,
+  T extends UnitType = UnitType,
+  R extends UnitRef< D > = UnitRef< D >
+> = {
+  readonly type: T;
+  readonly id: R;
+  readonly dim: D;
+};
+
+export type DerivedUnitDef< R extends UnitRef > = UnitDef< R[ '__dim' ], R[ '__type' ], R >;
+
+export type UnitLike = UnitRef | UnitDef | string;
