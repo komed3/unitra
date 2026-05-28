3ae5ad5970963606d69202355004df59e942c06f
komed3 <pk97@outlook.de>
2026-05-28 20:38:19 +0200

    add quantity dict

---
+++ packages/dict/src/quantity.ts
@@ -0,0 +1,8 @@
+export enum Branch {
+  MECHANICS = 'mechanics',
+  ACOUSTICS = 'acoustics',
+  ELECTROMAGNETISM = 'electromagnetism',
+  OPTICS = 'optics',
+  THERMODYNAMICS = 'thermodynamics',
+  COMMON = 'common'
+}
