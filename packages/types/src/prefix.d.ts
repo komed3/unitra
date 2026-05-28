6963e9432346a6a038bb6c427606855a37d5b790
komed3 <pk97@outlook.de>
2026-05-28 20:04:50 +0200

    declare prefix definition

--- packages/types/src/prefix.d.ts
+++ packages/types/src/prefix.d.ts
@@ -1,3 +1,17 @@
+import type { Deprecated, Meta } from './common';
+
 export type PrefixRef< S extends string = string > = S & {
   readonly __brand: 'prefix';
 };
+
+export type PrefixDef< R extends PrefixRef = PrefixRef > = {
+  readonly id: R;
+  factor: number;
+  aliases?: ReadonlyArray< string >;
+  deprecated?: Deprecated< PrefixRef >;
+  meta: Meta;
+};
+
+export type DerivedPrefixDef< R extends PrefixRef > = PrefixDef< R >;
+
+export type PrefixLike = PrefixRef | PrefixDef | string;
