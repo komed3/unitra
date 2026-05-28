c238005401f4d5f1a1ab1f04a7ac8df75771ef6a
komed3 <pk97@outlook.de>
2026-05-28 20:40:09 +0200

    implement quantity types

---
+++ packages/types/src/quantity.d.ts
@@ -0,0 +1,33 @@
+import type { Branch } from '@unitra/dict/quantity';
+import type { Deprecated, Meta } from './common';
+import type { Dimension } from './dim';
+
+declare const quantityBrand: unique symbol;
+
+type QuantityBrand< D extends Dimension > = {
+  readonly dim: D;
+};
+
+export type QuantityRef<
+  D extends Dimension = Dimension,
+  S extends string = string
+> = S & {
+  readonly [ quantityBrand ]: QuantityBrand< D >;
+};
+
+export type QuantityDim< R extends QuantityRef > = R[ typeof quantityBrand ][ 'dim' ];
+
+export type QuantityDef<
+  D extends Dimension = Dimension,
+  R extends QuantityRef< D > = QuantityRef< D >
+> = {
+  readonly id: R;
+  readonly dim: D;
+  branch?: Branch;
+  deprecated?: Deprecated< QuantityRef< D > >;
+  meta: Meta;
+};
+
+export type DerivedQuantityDef< R extends QuantityRef > = QuantityDef< QuantityDim< R >, R >;
+
+export type QuantityLike = QuantityRef | QuantityDef | string;
