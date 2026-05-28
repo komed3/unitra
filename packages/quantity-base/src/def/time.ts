61b2fdb42fc8a61c37800e8b2a3d2b20b9976b42
komed3 <pk97@outlook.de>
2026-05-28 23:54:10 +0200

    Update time.ts

--- packages/quantity-base/src/def/time.ts
+++ packages/quantity-base/src/def/time.ts
@@ -1,4 +1,6 @@
 import { TimeDim } from '@unitra/dim/base';
-import { QuantityRef } from '@unitra/types/quantity';
+import { DerivedQuantityDef, QuantityRef } from '@unitra/types/quantity';
 
 export const time = 'time' as QuantityRef< typeof TimeDim, 'time' >;
+
+export default ( {} ) as const satisfies DerivedQuantityDef< typeof time >;
