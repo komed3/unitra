36fef6bfb6984819e731705f25e13088c713aabc
komed3 <pk97@outlook.de>
2026-05-29 00:57:27 +0200

    Create mole.ts

---
+++ packages/plugin/unit/base/src/def/mole.ts
@@ -0,0 +1,50 @@
+import { Format, Lang, SIType, System } from '@unitra/dict/common';
+import { UnitStatus, UnitType } from '@unitra/dict/unit';
+import { AmountOfSubstanceDim } from '@unitra/dim/base';
+import { amountOfSubstance } from '@unitra/quantity-base/def/amountOfSubstance';
+import type { DerivedUnitDef, UnitRef } from '@unitra/types/unit';
+
+export const mole = 'mol' as UnitRef< typeof AmountOfSubstanceDim, UnitType.NAMED, 'mol' >;
+
+export default ( {
+  type: UnitType.NAMED,
+  id: mole,
+  dim: AmountOfSubstanceDim,
+  structure: [],
+  conversion: {
+    type: 'identity'
+  },
+  prefixable: true,
+  aliases: [
+    'mole',
+    'moles'
+  ],
+  context: {
+    system: [
+      System.SI
+    ],
+    status: UnitStatus.ACTIVE,
+    si: SIType.BASE,
+    quantities: [
+      amountOfSubstance
+    ]
+  },
+  meta: {
+    symbol: [ {
+      id: 'mol',
+      canonical: true,
+      format: {
+        [ Format.PLAIN ]: 'mol',
+        [ Format.LATEX ]: '\\mathrm{mol}'
+      }
+    } ],
+    name: {
+      [ Lang.EN ]: [ 'mole', 'moles' ],
+      [ Lang.DE ]: [ 'Mol' ]
+    },
+    description: {
+      [ Lang.EN ]: 'SI unit of amount of substance',
+      [ Lang.DE ]: 'SI-Basiseinheit der Stoffmenge'
+    }
+  }
+} ) as const satisfies DerivedUnitDef< typeof mole >;
