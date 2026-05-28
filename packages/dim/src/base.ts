89612fb98a04350a5329a07d4f21a76aa6fe4bae
komed3 <pk97@outlook.de>
2026-05-28 17:51:57 +0200

    declare dim vectors

---
+++ packages/dim/src/base.ts
@@ -0,0 +1,7 @@
+export const TimeDim = [ 1, 0, 0, 0, 0, 0, 0 ] as const;
+export const LengthDim = [ 0, 1, 0, 0, 0, 0, 0 ] as const;
+export const MassDim = [ 0, 0, 1, 0, 0, 0, 0 ] as const;
+export const ElectricCurrentDim = [ 0, 0, 0, 1, 0, 0, 0 ] as const;
+export const TemperatureDim = [ 0, 0, 0, 0, 1, 0, 0 ] as const;
+export const AmountOfSubstanceDim = [ 0, 0, 0, 0, 0, 1, 0 ] as const;
+export const LuminousIntensityDim = [ 0, 0, 0, 0, 0, 0, 1 ] as const;
