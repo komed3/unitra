0bc0598b19dea8a00bc1fd38dc62bee9a3ece1c4
komed3 <pk97@outlook.de>
2026-05-28 19:08:06 +0200

    declare common meta types

--- packages/types/src/common.d.ts
+++ packages/types/src/common.d.ts
@@ -1,4 +1,4 @@
-import type { Format, Lang } from '@unitra/dict/common';
+import type { Format, Lang, System } from '@unitra/dict/common';
 import type { PrefixRef } from './prefix';
 
 export type LangGroup< T = unknown > =
@@ -10,4 +10,31 @@ export type FormatGroup< T = unknown > =
 
 export type Modifier = {
   prefix?: PrefixRef;
+};
+
+export type Symbol = {
+  readonly id: string;
+  canonical?: boolean;
+  deprecated?: boolean;
+  context?: {
+    system?: ReadonlyArray< System >;
+    lang?: Lang;
+  };
+  format: FormatGroup< string >;
+};
+
+export type Formula = {
+  format: FormatGroup< string >;
+};
+
+export type Name = string | readonly [
+  singular: string,
+  plural?: string
+];
+
+export type Meta = {
+  symbol: Symbol[];
+  formula?: Formula;
+  name?: LangGroup< Name >;
+  description?: LangGroup< string >;
 };
