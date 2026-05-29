678c8a0d64de4f0bd811574baf9f71e86c813b09
komed3 <pk97@outlook.de>
2026-05-29 23:43:40 +0200

    declare SI base unit registry

--- packages/plugin/unit/si-base/src/index.ts
+++ packages/plugin/unit/si-base/src/index.ts
@@ -1,1 +1,19 @@
-export {};
+import type { UnitRegistry } from '@unitra/types/registry';
+
+import ampereDef, { ampere } from './def/ampere';
+import candelaDef, { candela } from './def/candela';
+import gramDef, { gram } from './def/gram';
+import kelvinDef, { kelvin } from './def/kelvin';
+import meterDef, { meter } from './def/meter';
+import moleDef, { mole } from './def/mole';
+import secondDef, { second } from './def/second';
+
+export default ( {
+  [ ampere ]: ampereDef,
+  [ candela ]: candelaDef,
+  [ gram ]: gramDef,
+  [ kelvin ]: kelvinDef,
+  [ meter ]: meterDef,
+  [ mole ]: moleDef,
+  [ second ]: secondDef
+} ) as const satisfies UnitRegistry;
