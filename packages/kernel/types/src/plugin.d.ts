7dc37263092de054ff30e3091efaa1b0f32341d9
komed3 <pk97@outlook.de>
2026-05-29 19:24:59 +0200

    simplify

--- packages/kernel/types/src/plugin.d.ts
+++ packages/kernel/types/src/plugin.d.ts
@@ -2,30 +2,10 @@ export type SemverVersion = `${ number }.${ number }.${ number }${ string }`;
 export type SemverOperator = '^' | '~' | '>' | '>=' | '<' | '<=' | '=';
 export type SemverRange = `${ SemverOperator }${ SemverVersion }` | `${ SemverVersion }`;
 
-declare const pluginBrand: unique symbol;
+export type DependencyMap = Readonly< Record< string, SemverRange > >;
 
-type PluginBrand<
-  S extends string,
-  V extends SemverRange
-> = {
-  readonly id: S;
-  readonly version: V;
-};
-
-export type PluginRef<
-  S extends string = string,
-  V extends SemverRange = SemverRange
-> = S & {
-  readonly [ pluginBrand ]: PluginBrand< S, V >;
-};
-
-export type PluginId< R extends PluginRef > = R[ typeof pluginBrand ][ 'id' ];
-export type PluginVersion< R extends PluginRef > = R[ typeof pluginBrand ][ 'version' ];
-
-export type PluginDef<
-  S extends string = string,
-  V extends SemverRange = SemverRange
-> = {
-  readonly id: S;
-  readonly version: V;
+export type PluginDefinition = {
+  readonly id: string;
+  readonly version: string;
+  dependencies?: DependencyMap;
 };
