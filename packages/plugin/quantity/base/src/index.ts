3b2684208e890cdcb3b6d28d361d50b7675d9bf2
komed3 <pk97@outlook.de>
2026-05-29 23:51:09 +0200

    create base quantity registry

--- packages/plugin/quantity/base/src/index.ts
+++ packages/plugin/quantity/base/src/index.ts
@@ -1,1 +1,19 @@
-export {};
+import type { QuantityRegistry } from '@unitra/types/registry';
+
+import amountOfSubstanceDef, { amountOfSubstance } from './def/amountOfSubstance';
+import electricCurrentDef, { electricCurrent } from './def/electricCurrent';
+import lengthDef, { length } from './def/length';
+import luminousIntensityDef, { luminousIntensity } from './def/luminousIntensity';
+import massDef, { mass } from './def/mass';
+import temperatureDef, { temperature } from './def/temperature';
+import timeDef, { time } from './def/time';
+
+export default ( {
+  [ amountOfSubstance ]: amountOfSubstanceDef,
+  [ electricCurrent ]: electricCurrentDef,
+  [ length ]: lengthDef,
+  [ luminousIntensity ]: luminousIntensityDef,
+  [ mass ]: massDef,
+  [ temperature ]: temperatureDef,
+  [ time ]: timeDef
+} ) as const satisfies QuantityRegistry;
