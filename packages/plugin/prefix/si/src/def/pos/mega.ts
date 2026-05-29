de033159e8163cce214ed338fc6e45f89e533b61
komed3 <pk97@outlook.de>
2026-05-29 10:02:56 +0200

    add more SI prefixes

---
+++ packages/plugin/prefix/si/src/def/pos/mega.ts
@@ -0,0 +1,28 @@
+import { Format, Lang } from '@unitra/dict/common';
+import type { DerivedPrefixDef, PrefixRef } from '@unitra/types/prefix';
+
+export const mega = 'M' as PrefixRef< 'M' >;
+
+export default ( {
+  id: mega,
+  factor: 1e6,
+  aliases: [ 'mega' ],
+  meta: {
+    symbol: [ {
+      id: 'M',
+      canonical: true,
+      format: {
+        [ Format.PLAIN ]: 'M',
+        [ Format.LATEX ]: '\\mathrm{M}'
+      }
+    } ],
+    name: {
+      [ Lang.EN ]: 'mega',
+      [ Lang.DE ]: 'Mega'
+    },
+    description: {
+      [ Lang.EN ]: 'unit prefix with the factor 1e6',
+      [ Lang.DE ]: 'Einheitenpräfix mit dem Faktor 1e6'
+    }
+  }
+} ) as const satisfies DerivedPrefixDef< typeof mega >;
