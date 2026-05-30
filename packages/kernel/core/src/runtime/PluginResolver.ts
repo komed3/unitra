c546a785a8888ee4696d85eba53cac7208552db2
komed3 <pk97@outlook.de>
2026-05-30 15:55:15 +0200

    update plugin resolver

--- packages/kernel/core/src/runtime/PluginResolver.ts
+++ packages/kernel/core/src/runtime/PluginResolver.ts
@@ -1,5 +1,9 @@
+import { PluginLoader } from './PluginLoader';
+
 export class PluginResolver {
   public static resolve () : ResolvedPlugins {
     const catalog = PluginLoader.all();
   }
 }
+
+export const resolvePlugins = () => PluginResolver.resolve();
