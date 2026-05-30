dd46fedaafc93f11bb2b42a97cf83cbc1b221b46
komed3 <pk97@outlook.de>
2026-05-30 17:58:11 +0200

    declare helper type

--- packages/kernel/core/src/runtime/PluginResolver.ts
+++ packages/kernel/core/src/runtime/PluginResolver.ts
@@ -1,6 +1,8 @@
 import type { ParsedSemverVersion, PluginDefinition, SemverOperator } from '@unitra/types/plugin';
 import { PluginLoader } from './PluginLoader';
 
+type Selection = Map< string, PluginDefinition >;
+
 export class PluginResolver {
   private static parse ( version: string ) : ParsedSemverVersion {
     const [ major, minor, patch ] = version.split( '-' )[ 0 ].split( '.' ).map( Number );
