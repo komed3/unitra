2d4f7bb9c1b18cfe044da608fe3848fa09b8ec24
komed3 <pk97@outlook.de>
2026-05-30 23:33:34 +0200

    prep public API for plugin resolver

--- packages/kernel/core/src/runtime/PluginResolver.ts
+++ packages/kernel/core/src/runtime/PluginResolver.ts
@@ -1,1 +1,10 @@
-export class PluginResolver {}
+import type { PluginResolveResult } from '@unitra/types/plugin';
+import { PluginRegistry } from './PluginRegistry';
+
+export class PluginResolver {
+  public static resolve () : PluginResolveResult {
+    const catalog = PluginRegistry.catalog;
+  }
+}
+
+export const resolvePlugins = () => PluginResolver.resolve();
