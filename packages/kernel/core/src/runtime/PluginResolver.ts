ccd4b4ad5a71edfd9026d748f0f14ad24912c259
komed3 <pk97@outlook.de>
2026-05-30 23:37:11 +0200

    parse (semver) version

--- packages/kernel/core/src/runtime/PluginResolver.ts
+++ packages/kernel/core/src/runtime/PluginResolver.ts
@@ -1,7 +1,14 @@
-import type { PluginResolveResult } from '@unitra/types/plugin';
+import type { ParsedSemverVersion, PluginResolveResult, SemverVersion } from '@unitra/types/plugin';
 import { PluginRegistry } from './PluginRegistry';
 
 export class PluginResolver {
+  private static parse ( version: SemverVersion ) : ParsedSemverVersion {
+    const [ semver, tag ] = version.split( '-' );
+    const [ major, minor, patch ] = semver.split( '.' ).map( Number );
+
+    return { major, minor, patch, tag };
+  }
+
   public static resolve () : PluginResolveResult {
     const catalog = PluginRegistry.catalog;
   }
