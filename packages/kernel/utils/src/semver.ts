b4afa493b601345ca74d06a029adbdcc073e3011
komed3 <pk97@outlook.de>
2026-05-31 09:52:39 +0200

    parse semver version

--- packages/kernel/utils/src/semver.ts
+++ packages/kernel/utils/src/semver.ts
@@ -1,3 +1,5 @@
+import { ParsedSemverVersion, SemverVersion } from '@unitra/types/semver';
+
 export class Semver {
   private static readonly OPERATORS = {
     '=': ( cmp: number ) => cmp === 0,
@@ -6,4 +8,19 @@ export class Semver {
     '<': ( cmp: number ) => cmp < 0,
     '<=': ( cmp: number ) => cmp <= 0
   } as const;
+
+  public static parse ( version: SemverVersion ) : ParsedSemverVersion {
+    const [ semver, tag ] = version.split( '-', 2 );
+    const parts = semver.split( '.' );
+
+    if ( parts.length !== 3 )
+      throw new Error( `Invalid semantic version "${ version }".` );
+
+    const [ major, minor, patch ] = parts.map( Number );
+
+    if ( Number.isNaN( major ) || Number.isNaN( minor ) || Number.isNaN( patch ) )
+      throw new Error( `Invalid semantic version "${ version }".` );
+
+    return [ major, minor, patch, tag ];
+  }
 }
