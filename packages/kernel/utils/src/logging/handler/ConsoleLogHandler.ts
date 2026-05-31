ff7d10a7a63980dc7fbc11d571ddda0d376a4645
komed3 <pk97@outlook.de>
2026-05-31 13:28:18 +0200

    implement console log handler

--- packages/kernel/utils/src/logging/handler/ConsoleLogHandler.ts
+++ packages/kernel/utils/src/logging/handler/ConsoleLogHandler.ts
@@ -1,1 +1,17 @@
-export class ConsoleLogHandler {}
+import type { ILogHandler, LogEntry } from '@unitra/types/logging';
+import { LogHandler } from './LogHandler';
+import { LogLevel } from '@unitra/dict/logging';
+
+export class ConsoleLogHandler extends LogHandler implements ILogHandler {
+  private static readonly METHODS = {
+    [ LogLevel.DEBUG ]: console.debug,
+    [ LogLevel.LOG ]: console.log,
+    [ LogLevel.WARN ]: console.warn,
+    [ LogLevel.ERROR ]: console.error,
+    [ LogLevel.NONE ]: () => undefined
+  } as const;
+
+  public override write ( entry: LogEntry ) : void {
+    ConsoleLogHandler.METHODS[ entry.level ]( this.formatter.format( entry ) );
+  }
+}
