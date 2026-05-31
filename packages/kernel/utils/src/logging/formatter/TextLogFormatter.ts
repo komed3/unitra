0f4a5c0873f069fab6ce2c130d8f089a8c1cdc5a
komed3 <pk97@outlook.de>
2026-05-31 11:54:05 +0200

    implement TextLogFormatter class

---
+++ packages/kernel/utils/src/logging/formatter/TextLogFormatter.ts
@@ -0,0 +1,14 @@
+import { LogLevel } from '@unitra/dict/logging';
+import type { ILogFormatter, LogEntry } from '@unitra/types/logging';
+import { LogFormatter } from './LogFormatter';
+
+export class TextLogFormatter extends LogFormatter implements ILogFormatter {
+  public format ( entry: LogEntry ) : string {
+    const timestamp = new Date( entry.timestamp ).toISOString();
+    const level = LogLevel[ entry.level ];
+
+    let output = `[${ timestamp }] [${ level }] [${ entry.source }] ${ entry.message }`;
+    if ( entry.data !== undefined ) output += '\n' + JSON.stringify( entry.data, null, 2 );
+    return output;
+  }
+}
