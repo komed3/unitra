d56e1b7dfb20485bce1ba329b01ddd70e06d080c
komed3 <pk97@outlook.de>
2026-05-31 10:57:25 +0200

    implement memory handler

--- packages/kernel/utils/src/logging/handler/MemoryLogHandler.ts
+++ packages/kernel/utils/src/logging/handler/MemoryLogHandler.ts
@@ -1,1 +1,33 @@
-export class MemoryLogHandler {}
+import type { LogEntry, LogHandler } from '@unitra/types/logging';
+
+export class MemoryLogHandler {
+  public readonly entries: LogEntry[] = [];
+
+  public readonly handler: LogHandler = entry => {
+    this.entries.push( entry );
+  };
+
+  public get all () : LogEntry[] {
+    return this.entries;
+  }
+
+  public get logs () : LogEntry[] {
+    return this.entries.filter( entry => entry.level === 1 );
+  }
+
+  public get warnings () : LogEntry[] {
+    return this.entries.filter( entry => entry.level === 2 );
+  }
+
+  public get errors () : LogEntry[] {
+    return this.entries.filter( entry => entry.level === 3 );
+  }
+
+  public get size () : number {
+    return this.entries.length;
+  }
+
+  public clear () : void {
+    this.entries.length = 0;
+  }
+}
