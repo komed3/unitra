e3f93fecd3b74519392e9df850b99bb0ae18d4e3
komed3 <pk97@outlook.de>
2026-05-31 10:54:10 +0200

    re-export logging capabilities

--- packages/kernel/utils/src/logging/index.ts
+++ packages/kernel/utils/src/logging/index.ts
@@ -1,1 +1,18 @@
-export {};
+import { Logging } from './Logging';
+export default Logging;
+
+export { ConsoleLogHandler } from './handler/ConsoleLogHandler';
+export { FileLogHandler } from './handler/FileLogHandler';
+export { MemoryLogHandler } from './handler/MemoryLogHandler';
+
+export const debug = ( source: string, message: string, data?: unknown ) =>
+  Logging.debug( source, message, data );
+
+export const log = ( source: string, message: string, data?: unknown ) =>
+  Logging.log( source, message, data );
+
+export const warn = ( source: string, message: string, data?: unknown ) =>
+  Logging.warn( source, message, data );
+
+export const error = ( source: string, message: string, data?: unknown ) =>
+  Logging.error( source, message, data );
