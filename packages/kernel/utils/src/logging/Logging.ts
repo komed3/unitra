a411f20937d309f169ed33fee82f42ac73b9edde
komed3 <pk97@outlook.de>
2026-05-31 10:40:41 +0200

    create logging cls

--- packages/kernel/utils/src/logging/Logging.ts
+++ packages/kernel/utils/src/logging/Logging.ts
@@ -1,1 +1,7 @@
-export class Logging {}
+import { LogLevel } from '@unitra/dict/logging';
+import type { LogHandler } from '@unitra/types/logging';
+
+export class Logging {
+  private static level = LogLevel.LOG;
+  private static handlers = new Set< LogHandler >();
+}
