7a292ef3166f571a454e27f6ac6f230261b1539b
komed3 <pk97@outlook.de>
2026-05-31 10:00:41 +0200

    compare semver versions

--- packages/kernel/utils/src/semver.ts
+++ packages/kernel/utils/src/semver.ts
@@ -32,4 +32,10 @@ export class Semver {
 
     return { operator, version: this.parse( version ) };
   }
+
+  public static compare ( a: ParsedSemverVersion, b: ParsedSemverVersion ) : number {
+    if ( a[ 0 ] !== b[ 0 ] ) return a[ 0 ] - b[ 0 ];
+    if ( a[ 1 ] !== b[ 1 ] ) return a[ 1 ] - b[ 1 ];
+    return a[ 2 ] - b[ 2 ];
+  }
 }
