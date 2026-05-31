ca27946ba9a4da0b93dc0765c320ae187ceb4f8d
komed3 <pk97@outlook.de>
2026-05-31 10:43:13 +0200

    get / set log level (WARN is default)

--- packages/kernel/utils/src/logging/Logging.ts
+++ packages/kernel/utils/src/logging/Logging.ts
@@ -2,6 +2,14 @@ import { LogLevel } from '@unitra/dict/logging';
 import type { LogHandler } from '@unitra/types/logging';
 
 export class Logging {
-  private static level = LogLevel.LOG;
+  private static _level = LogLevel.WARN;
   private static handlers = new Set< LogHandler >();
+
+  public static get level () : LogLevel {
+    return this._level;
+  }
+
+  public static set level ( level: LogLevel ) {
+    this._level = level;
+  }
 }
