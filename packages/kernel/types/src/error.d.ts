fa7fa8f9481384418686414b7fe3bd323f95706d
komed3 <pk97@outlook.de>
2026-06-01 09:31:04 +0200

    declare the SerializedError type

--- packages/kernel/types/src/error.d.ts
+++ packages/kernel/types/src/error.d.ts
@@ -5,3 +5,12 @@ export type UnitraErrorOptions = {
   data?: unknown;
   cause?: unknown;
 };
+
+export type SerializedError = {
+  name: string;
+  code?: UnitraErrorCode;
+  message: string;
+  stack?: string;
+  data?: unknown;
+  cause?: SerializedError;
+};
