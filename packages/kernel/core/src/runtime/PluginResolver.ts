d3de23dcb889d8d660b1084fd0b6302567db94c4
komed3 <pk97@outlook.de>
2026-06-01 23:25:35 +0200

    create public API

--- packages/kernel/core/src/runtime/PluginResolver.ts
+++ packages/kernel/core/src/runtime/PluginResolver.ts
@@ -1,5 +1,13 @@
+import { PluginResolveResult } from '@unitra/types/plugin';
 import Logging from '@unitra/utils/logging';
 
 export class PluginResolver {
   private static readonly logger = Logging.createSource( 'plugin-resolver' );
+
+  private static cacheHash = '';
+  private static cache: PluginResolveResult | null = null;
+
+  public static resolve () : PluginResolveResult {}
 }
+
+export const resolvePlugins = () => PluginResolver.resolve();
