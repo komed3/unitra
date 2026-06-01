ff9c25065602330311d8a3362525f110e34cd290
komed3 <pk97@outlook.de>
2026-06-01 20:14:13 +0200

    override code in plugin error subclass

--- packages/kernel/utils/src/error/errors/PluginError.ts
+++ packages/kernel/utils/src/error/errors/PluginError.ts
@@ -1,9 +1,6 @@
-import type { UnitraErrorOptions } from '@unitra/types/error';
 import { UnitraErrorCode } from '@unitra/dict/unitra';
 import { UnitraError } from '../UnitraError';
 
 export class PluginError extends UnitraError {
-  public constructor ( message: string, options: UnitraErrorOptions = {} ) {
-    super( message, { ...options, code: UnitraErrorCode.PLUGIN_ERROR } );
-  }
+  public override readonly code = UnitraErrorCode.PLUGIN_ERROR;
 }
