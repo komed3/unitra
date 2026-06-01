1faa589d92867ac39bd4c9f84a92ba45daabfe52
komed3 <pk97@outlook.de>
2026-06-01 11:08:19 +0200

    declare error exports

--- packages/kernel/utils/src/error/index.ts
+++ packages/kernel/utils/src/error/index.ts
@@ -1,1 +1,7 @@
-export {};
+import { UnitraError } from './UnitraError';
+export default UnitraError;
+
+export * from './ErrorFormatter';
+export * from './serializeError';
+
+export * from './errors/PluginError';
