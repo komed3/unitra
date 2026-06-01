59f890564ddb97f1fcd3b2607ed0499109792056
komed3 <pk97@outlook.de>
2026-06-01 10:23:25 +0200

    declare error formatter class

---
+++ packages/kernel/utils/src/error/ErrorFormatter.ts
@@ -0,0 +1,16 @@
+import type { ErrorFormatterConfig, SerializedError } from '@unitra/types/error';
+import { serializeError } from './serializeError';
+
+export class ErrorFormatter {
+  private static readonly DEFAULT_CONFIG: Required< ErrorFormatterConfig > = {
+    showCode: true, showData: true, showStack: true, showCauses: true, indent: '   '
+  };
+
+  private readonly options: Required< ErrorFormatterConfig >;
+  private readonly serialized: SerializedError | unknown;
+
+  constructor ( private readonly error: unknown, options?: ErrorFormatterConfig ) {
+    this.options = { ...ErrorFormatter.DEFAULT_CONFIG, ...options };
+    this.serialized = serializeError( error );
+  }
+}
