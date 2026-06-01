31f9fb7850645c741459af50b041d3143a4964db
komed3 <pk97@outlook.de>
2026-06-01 15:09:41 +0200

    implement basic error handling

--- packages/kernel/utils/src/error/errorUtils.ts
+++ packages/kernel/utils/src/error/errorUtils.ts
@@ -1,10 +1,13 @@
 import { UnitraError } from './UnitraError';
 
-export const catchToError = < T > ( fn: () => T, safe: boolean = true ) => {
-  try { return fn() } catch ( err: unknown ) {
-    const error = UnitraError.from( err );
+export const handleError = ( err: unknown, safe: boolean = true ) => {
+  const error = UnitraError.from( err );
 
-    if ( safe ) throw error.format();
-    error.log();
-  }
+  if ( safe ) throw error.format();
+  error.log();
+};
+
+export const catchToError = < T > ( fn: () => T, safe: boolean = true ) => {
+  try { return fn() }
+  catch ( err ) { handleError( err, safe ) }
 }
