e3597f3d0cfcd91c88c7c4caf260f420a9fb5867
komed3 <pk97@outlook.de>
2026-06-01 23:39:32 +0200

    check for cached result

--- packages/kernel/core/src/runtime/PluginResolver.ts
+++ packages/kernel/core/src/runtime/PluginResolver.ts
@@ -1,13 +1,34 @@
-import { PluginResolveResult } from '@unitra/types/plugin';
+import { PluginCatalog, PluginResolveResult } from '@unitra/types/plugin';
 import Logging from '@unitra/utils/logging';
+import { PluginRegistry } from './PluginRegistry';
 
 export class PluginResolver {
-  private static readonly logger = Logging.createSource( 'plugin-resolver' );
+  private static readonly log = Logging.createSource( 'plugin-resolver' );
 
   private static cacheHash = '';
   private static cache: PluginResolveResult | null = null;
 
-  public static resolve () : PluginResolveResult {}
+  private static hash ( catalog: PluginCatalog ) : string {
+    let hash = '';
+
+    for ( const [ id, list ] of catalog ) {
+      hash += id + ':';
+      for ( const p of list ) hash += p.version + '|';
+      hash += ';';
+    }
+
+    return hash;
+  }
+
+  public static resolve () : PluginResolveResult {
+    const catalog = PluginRegistry.catalog();
+
+    const hash = this.hash( catalog );
+    if ( this.cache && this.cacheHash === hash ) {
+      this.log.debug( 'cache hit' );
+      return this.cache;
+    }
+  }
 }
 
 export const resolvePlugins = () => PluginResolver.resolve();
