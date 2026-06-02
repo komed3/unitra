b8cb36ee626b1998dc4537acd58af38945fe924c
komed3 <pk97@outlook.de>
2026-06-02 13:50:00 +0200

    hook def has handler + prio

--- packages/kernel/types/src/hook.d.ts
+++ packages/kernel/types/src/hook.d.ts
@@ -1,1 +1,6 @@
 export type HookHandler< C, I = void, O = void > = ( ctx: C, input?: I ) => O;
+
+export type HookDefinition< C, I = void, O = void > = {
+  handler: HookHandler< C, I, O >;
+  priority?: number;
+};
