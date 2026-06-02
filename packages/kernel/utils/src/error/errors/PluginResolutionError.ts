56c3f77a216f710f34d06a10d702d9d567285b8b
komed3 <pk97@outlook.de>
2026-06-02 10:35:31 +0200

    implement PluginResolutionError summary getter

--- packages/kernel/utils/src/error/errors/PluginResolutionError.ts
+++ packages/kernel/utils/src/error/errors/PluginResolutionError.ts
@@ -6,16 +6,16 @@ export class PluginResolutionError extends UnitraError< ErrorType > {
   public override readonly code = UnitraErrorCode.PLUGIN_RESOLUTION_ERROR;
 
   public override get summary () : string {
-    let out = '';
+    const parts: string[] = [];
 
     if ( this.data ) {
       const { missing, conflicts, cycles } = this.data;
 
-      if ( missing ) out += `\n\nMissing dependencies:\n└─ ${ missing.join( '\n└─ ' ) }`;
-      if ( conflicts ) out += `\n\nVersion conflicts:\n└─ ${ conflicts.join( '\n└─ ' ) }`;
-      if ( cycles ) out += `\n\nDependency cycles:\n└─ ${ cycles.join( '\n└─ ' ) }`;
+      if ( missing?.length ) parts.push( `Missing dependencies:\n└─ ${ missing.join( '\n└─ ' ) }` );
+      if ( conflicts?.length ) parts.push( `Version conflicts:\n└─ ${ conflicts.join( '\n└─ ' ) }` );
+      if ( cycles?.length ) parts.push( `Dependency cycles:\n└─ ${ cycles.join( '\n└─ ' ) }` );
     }
 
-    return out;
+    return parts.join( '\n\n' );
   }
 }
