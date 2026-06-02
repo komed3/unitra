9929d24cc6fa6d8792a73da560e7a51da80baa29
komed3 <pk97@outlook.de>
2026-06-03 01:06:34 +0200

    implement bulk import

--- packages/kernel/core/src/registry/BaseRegistry.ts
+++ packages/kernel/core/src/registry/BaseRegistry.ts
@@ -10,4 +10,12 @@ export abstract class BaseRegistry< Ref extends string, Def > {
   public has ( ref: Ref ) : boolean {
     return this.store.has( ref );
   }
+
+  public set < R extends Ref > ( ref: R, def: Def ) : void {
+    this.store.set( ref, def );
+  }
+
+  public bulk ( input: Iterable< [ Ref, Def ] > ) : void {
+    for ( const [ ref, def ] of input ) this.store.set( ref, def );
+  }
 }
