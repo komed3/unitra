4a819f436de1bf5d9cbbd6420794505b9abec72d
komed3 <pk97@outlook.de>
2026-06-02 13:52:00 +0200

    export hook types

--- packages/kernel/core/src/kernel/hook/HookEngine.ts
+++ packages/kernel/core/src/kernel/hook/HookEngine.ts
@@ -1,1 +1,3 @@
+type Pipeline< C extends {} > = ( ctx: C, input?: unknown ) => unknown;
+
 export class HookEngine {}
--- packages/kernel/types/src/hook.d.ts
+++ packages/kernel/types/src/hook.d.ts
@@ -1,6 +1,6 @@
-export type HookHandler< C, I = void, O = void > = ( ctx: C, input?: I ) => O;
+export type HookHandler< C extends {}, I = void, O = void > = ( ctx: C, input?: I ) => O;
 
-export type HookDefinition< C, I = void, O = void > = {
+export type HookDefinition< C extends {}, I = void, O = void > = {
   handler: HookHandler< C, I, O >;
   priority?: number;
 };
