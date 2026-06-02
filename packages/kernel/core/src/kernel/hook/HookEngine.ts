5c520875e30837785e16bfbda3ee9557a21a7515
komed3 <pk97@outlook.de>
2026-06-02 16:37:51 +0200

    add new hook

--- packages/kernel/core/src/kernel/hook/HookEngine.ts
+++ packages/kernel/core/src/kernel/hook/HookEngine.ts
@@ -1,9 +1,25 @@
-import type { HookDef, HookId, HookPipeline } from '@unitra/types/hook';
+import type { HookCtx, HookDef, HookHandler, HookId, HookImplMap, HookIn, HookOut, HookPipeline, HookSpec } from '@unitra/types/hook';
 import Logging from '@unitra/utils/logging';
 
 export class HookEngine {
   private static readonly log = Logging.createSource( 'hook' );
 
-  private readonly hooks = new Map< HookId, ReadonlyArray< HookDef< any > > >();
+  private readonly hooks = new Map< HookId, HookDef< any >[] >();
   private readonly cache = new Map< HookId, HookPipeline< any > >();
+
+  constructor () {}
+
+  public add < K extends HookId > ( id: K, handler: HookHandler< HookSpec< K > >, priority?: number ) : void {
+    const list = this.hooks.get( id );
+    if ( list ) list.push( { handler, priority } );
+    else this.hooks.set( id, [ { handler, priority } ] );
+
+    this.cache.delete( id );
+  }
+
+  public merge ( hooks: HookImplMap ) : void {}
+
+  public run < K extends HookId > ( id: K, ctx: HookCtx< K >, input?: HookIn< K > ) : HookOut< K > {
+    return undefined as HookOut< K >;
+  }
 }
