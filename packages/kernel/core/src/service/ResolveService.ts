24f715332d58e463ce698492000b212272c8a473
komed3 <pk97@outlook.de>
2026-06-03 17:41:42 +0200

    implement registry getter

--- packages/kernel/core/src/service/ResolveService.ts
+++ packages/kernel/core/src/service/ResolveService.ts
@@ -1,5 +1,13 @@
+import type { AnyRef, IRegistry, RegistryKey } from '@unitra/types/registry';
 import type { ServicesContext } from '@unitra/types/service';
 
-export class Resolve {
-  constructor ( private readonly ctx: ServicesContext ) {}
+export class Resolve< Ref extends AnyRef, Def extends { id: Ref } > {
+  private get registry () : IRegistry< Ref > {
+    return this.ctx.services.registry[ this.key ] as unknown as IRegistry< Ref >;
+  }
+
+  constructor (
+    private readonly ctx: ServicesContext,
+    private readonly key: RegistryKey
+  ) {}
 }
