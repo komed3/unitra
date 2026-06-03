7fae048727f9c8cbc23aca614d4d4d30c9f126d9
komed3 <pk97@outlook.de>
2026-06-03 17:48:58 +0200

    implement resolver service

--- packages/kernel/core/src/service/ResolveService.ts
+++ packages/kernel/core/src/service/ResolveService.ts
@@ -1,7 +1,7 @@
-import type { AnyRef, IRegistry, RegistryKey } from '@unitra/types/registry';
+import type { AnyRef, IRegistry, RegistryDef, RegistryKey } from '@unitra/types/registry';
 import type { ServicesContext } from '@unitra/types/service';
 
-export class Resolve< Ref extends AnyRef, Def extends { id: Ref } > {
+export class Resolve< Ref extends AnyRef, Like > {
   private get registry () : IRegistry< Ref > {
     return this.ctx.services.registry[ this.key ] as unknown as IRegistry< Ref >;
   }
@@ -10,4 +10,13 @@ export class Resolve< Ref extends AnyRef, Def extends { id: Ref } > {
     private readonly ctx: ServicesContext,
     private readonly key: RegistryKey
   ) {}
+
+  public toRef < R extends Ref = Ref > ( value: Like ) : R {
+    this.ctx.services.assert[ this.key ].assertRef( value );
+    return value as unknown as R;
+  }
+
+  public toDef < R extends Ref = Ref > ( value: Like ) : RegistryDef< R > {
+    return this.registry.get( this.toRef( value ) ) as RegistryDef< R >;
+  }
 }
