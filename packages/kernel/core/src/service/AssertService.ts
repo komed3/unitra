5c59edd5d44e8fbe5e7a98cfad6ea6ac3235ec33
komed3 <pk97@outlook.de>
2026-06-03 11:04:01 +0200

    rework asserter (reduce boiler plate, fix error msg)

--- packages/kernel/core/src/service/AssertService.ts
+++ packages/kernel/core/src/service/AssertService.ts
@@ -1,16 +1,21 @@
 import type { ConstantDef, ConstantRef } from '@unitra/types/constant';
 import type { PrefixDef, PrefixRef } from '@unitra/types/prefix';
 import type { QuantityDef, QuantityRef } from '@unitra/types/quantity';
-import type { AnyRef, IRegistry } from '@unitra/types/registry';
+import type { AnyRef, IRegistry, RegistryKey } from '@unitra/types/registry';
 import type { IAssert, IAssertService, ServicesContext } from '@unitra/types/service';
 import type { UnitDef, UnitRef } from '@unitra/types/unit';
 import { AssertDefError, AssertRefError } from '@unitra/utils/error';
 import { safeJsonStringify } from '@unitra/utils/helper';
 
-export abstract class Assert< Ref extends AnyRef, Def extends { id: Ref } > implements IAssert< Ref, Def > {
-  protected abstract get registry () : IRegistry< Ref >;
+export class Assert< Ref extends AnyRef, Def extends { id: Ref } > implements IAssert< Ref, Def > {
+  private get registry () : IRegistry< Ref > {
+    return this.ctx.services.registry[ this.key ] as unknown as IRegistry< Ref >;
+  }
 
-  constructor ( protected readonly ctx: ServicesContext ) {}
+  constructor (
+    private readonly ctx: ServicesContext,
+    private readonly key: RegistryKey
+  ) {}
 
   public isRef ( value: unknown ) : value is Ref {
     return typeof value === 'string' && this.registry.has( value as Ref );
@@ -23,46 +28,22 @@ export abstract class Assert< Ref extends AnyRef, Def extends { id: Ref } > impl
 
   public assertRef ( value: unknown ) : asserts value is Ref {
     if ( ! this.isRef( value ) ) throw new AssertRefError(
-      `expected a reference, but got ${ safeJsonStringify( value ) }`,
+      `expected a ${ this.key } reference, but got ${ safeJsonStringify( value ) }`,
       { data: { value } }
     );
   }
 
   public assertDef ( value: unknown ) : asserts value is Def {
     if ( ! this.isDef( value ) ) throw new AssertDefError(
-      `expected a definition object, but got ${ safeJsonStringify( value ) }`,
+      `expected a ${ this.key } definition, but got ${ safeJsonStringify( value ) }`,
       { data: { value } }
     );
   }
 }
 
-export class AssertPrefix extends Assert< PrefixRef, PrefixDef > {
-  protected override get registry () : IRegistry< PrefixRef > {
-    return this.ctx.services.registry.prefix;
-  }
-}
-
-export class AssertQuantity extends Assert< QuantityRef, QuantityDef > {
-  protected override get registry () : IRegistry< QuantityRef > {
-    return this.ctx.services.registry.quantity;
-  }
-}
-
-export class AssertUnit extends Assert< UnitRef, UnitDef > {
-  protected override get registry () : IRegistry< UnitRef > {
-    return this.ctx.services.registry.unit;
-  }
-}
-
-export class AssertConstant extends Assert< ConstantRef, ConstantDef > {
-  protected override get registry () : IRegistry< ConstantRef > {
-    return this.ctx.services.registry.constant;
-  }
-}
-
 export const createAssertService = ( ctx: ServicesContext ) : IAssertService => ( {
-  prefix: new AssertPrefix( ctx ),
-  quantity: new AssertQuantity( ctx ),
-  unit: new AssertUnit( ctx ),
-  constant: new AssertConstant( ctx )
+  prefix: new Assert< PrefixRef, PrefixDef >( ctx, 'prefix' ),
+  quantity: new Assert< QuantityRef, QuantityDef >( ctx, 'quantity' ),
+  unit: new Assert< UnitRef, UnitDef >( ctx, 'unit' ),
+  constant: new Assert< ConstantRef, ConstantDef >( ctx, 'constant' )
 } );
