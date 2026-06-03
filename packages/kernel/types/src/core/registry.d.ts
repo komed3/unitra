759372a2d3fcdb2d905c5c863a1de2edb576d9aa
komed3 <pk97@outlook.de>
2026-06-03 02:07:40 +0200

    declare registry types / use conditional type for strict ref<->def relationship

--- packages/kernel/types/src/core/registry.d.ts
+++ packages/kernel/types/src/core/registry.d.ts
@@ -1,1 +1,17 @@
-export {};
+import type { ConstantRef, DerivedConstantDef } from '../def/constant';
+import type { DerivedPrefixDef, PrefixRef } from '../def/prefix';
+import type { DerivedQuantityDef, QuantityRef } from '../def/quantity';
+import type { DerivedUnitDef, UnitRef } from '../def/unit';
+
+export type AnyRef =
+  | UnitRef
+  | PrefixRef
+  | QuantityRef
+  | ConstantRef;
+
+export type RegistryDef< R extends AnyRef > =
+  R extends UnitRef ? DerivedUnitDef< R > :
+  R extends PrefixRef ? DerivedPrefixDef< R > :
+  R extends QuantityRef ? DerivedQuantityDef< R > :
+  R extends ConstantRef ? DerivedConstantDef< R > :
+  never;
