8053cb2edff13fb813ebf879ab1b370930ba00d8
komed3 <pk97@outlook.de>
2026-06-03 17:58:41 +0200

    create resolve service layer

--- packages/kernel/core/src/service/ResolveService.ts
+++ packages/kernel/core/src/service/ResolveService.ts
@@ -1,5 +1,9 @@
+import type { ConstantDef, ConstantRef } from '@unitra/types/constant';
+import type { PrefixDef, PrefixRef } from '@unitra/types/prefix';
+import type { QuantityDef, QuantityRef } from '@unitra/types/quantity';
 import type { AnyRef, IRegistry, RegistryDef, RegistryKey } from '@unitra/types/registry';
 import type { ServicesContext } from '@unitra/types/service';
+import type { UnitDef, UnitRef } from '@unitra/types/unit';
 
 export class Resolve< Ref extends AnyRef, Like > {
   private get registry () : IRegistry< Ref > {
@@ -12,11 +16,24 @@ export class Resolve< Ref extends AnyRef, Like > {
   ) {}
 
   public toRef < R extends Ref = Ref > ( value: Like ) : R {
+    if ( this.ctx.services.assert[ this.key ].isDef( value ) )
+      return value.id as unknown as R;
+
     this.ctx.services.assert[ this.key ].assertRef( value );
     return value as unknown as R;
   }
 
   public toDef < R extends Ref = Ref > ( value: Like ) : RegistryDef< R > {
-    return this.registry.get( this.toRef( value ) ) as RegistryDef< R >;
+    return (
+      this.ctx.services.assert[ this.key ].isDef( value ) ? value
+        : this.registry.get( this.toRef( value ) )
+    ) as RegistryDef< R >;
   }
 }
+
+export const createResolveService = ( ctx: ServicesContext ) => ( {
+  prefix: new Resolve< PrefixRef, PrefixDef >( ctx, 'prefix' ),
+  quantity: new Resolve< QuantityRef, QuantityDef >( ctx, 'quantity' ),
+  unit: new Resolve< UnitRef, UnitDef >( ctx, 'unit' ),
+  constant: new Resolve< ConstantRef, ConstantDef >( ctx, 'constant' )
+} );
