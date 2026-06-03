5dac9fab4aa36f6f4ff2e88a09cb387f0c318912
komed3 <pk97@outlook.de>
2026-06-03 02:08:25 +0200

    rewriting base registry class

--- packages/kernel/core/src/registry/BaseRegistry.ts
+++ packages/kernel/core/src/registry/BaseRegistry.ts
@@ -1,37 +1,5 @@
-export abstract class BaseRegistry< Ref extends string, Def > {
-  protected readonly store = new Map< Ref, Def >();
+import type { AnyRef, RegistryDef } from '@unitra/types/registry';
 
-  constructor () {}
-
-  public get < R extends Ref > ( ref: R ) : Def | undefined {
-    return this.store.get( ref );
-  }
-
-  public has ( ref: Ref ) : boolean {
-    return this.store.has( ref );
-  }
-
-  public set < R extends Ref > ( ref: R, def: Def ) : void {
-    this.store.set( ref, def );
-  }
-
-  public bulk ( input: Iterable< [ Ref, Def ] > ) : void {
-    for ( const [ ref, def ] of input ) this.store.set( ref, def );
-  }
-
-  public entries () : IterableIterator< [ Ref, Def ] > {
-    return this.store.entries();
-  }
-
-  public values () : IterableIterator< Def > {
-    return this.store.values();
-  }
-
-  public keys () : IterableIterator< Ref > {
-    return this.store.keys();
-  }
-
-  public filter ( predicate: ( def: Def ) => boolean ) : Def[] {
-    return [ ...this.store.values() ].filter( predicate );
-  }
+export abstract class BaseRegistry< Ref extends AnyRef > {
+  protected readonly store = new Map< Ref, RegistryDef< Ref > >();
 }
