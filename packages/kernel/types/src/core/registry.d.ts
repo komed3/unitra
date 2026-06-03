f7d03d8d9aed55c149081490bf0a5265a2445d04
komed3 <pk97@outlook.de>
2026-06-03 09:48:39 +0200

    Update registry.d.ts

--- packages/kernel/types/src/core/registry.d.ts
+++ packages/kernel/types/src/core/registry.d.ts
@@ -4,15 +4,15 @@ import type { DerivedQuantityDef, QuantityRef } from '../def/quantity';
 import type { DerivedUnitDef, UnitRef } from '../def/unit';
 
 export type AnyRef =
-  | UnitRef
   | PrefixRef
   | QuantityRef
+  | UnitRef
   | ConstantRef;
 
 export type RegistryDef< R extends AnyRef > =
-  R extends UnitRef ? DerivedUnitDef< R > :
   R extends PrefixRef ? DerivedPrefixDef< R > :
   R extends QuantityRef ? DerivedQuantityDef< R > :
+  R extends UnitRef ? DerivedUnitDef< R > :
   R extends ConstantRef ? DerivedConstantDef< R > :
   never;
 
