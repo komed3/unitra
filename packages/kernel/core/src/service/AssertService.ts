40948d30bfbc6e083eb223bf1f3c61ac8210b4b3
komed3 <pk97@outlook.de>
2026-06-03 10:29:59 +0200

    implement asserter for registries

--- packages/kernel/core/src/service/AssertService.ts
+++ packages/kernel/core/src/service/AssertService.ts
@@ -1,27 +1,54 @@
 import type { ConstantDef, ConstantRef } from '@unitra/types/constant';
 import type { PrefixDef, PrefixRef } from '@unitra/types/prefix';
 import type { QuantityDef, QuantityRef } from '@unitra/types/quantity';
+import type { AnyRef, IRegistry } from '@unitra/types/registry';
 import type { IAssert, IAssertService, ServicesContext } from '@unitra/types/service';
 import type { UnitDef, UnitRef } from '@unitra/types/unit';
 
-export abstract class Assert< Ref, Def > implements IAssert< Ref, Def > {
-  constructor ( private readonly ctx: ServicesContext ) {}
+export abstract class Assert< Ref extends AnyRef, Def extends { id: Ref } > implements IAssert< Ref, Def > {
+  protected abstract get registry () : IRegistry< Ref >;
 
-  public isRef ( value: unknown ) : value is Ref {}
+  constructor ( protected readonly ctx: ServicesContext ) {}
 
-  public isDef ( value: unknown ) : value is Def {}
+  public isRef ( value: unknown ) : value is Ref {
+    return typeof value === 'string' && this.registry.has( value as Ref );
+  }
+
+  public isDef ( value: unknown ) : value is Def {
+    return typeof value === 'object' && value !== null && 'id' in value &&
+      this.isRef( ( value as Def ).id );
+  }
 
   public assertRef ( value: unknown ) : asserts value is Ref {}
 
   public assertDef ( value: unknown ) : asserts value is Def {}
 }
 
-export class AssertPrefix extends Assert< PrefixRef, PrefixDef > {}
-export class AssertQuantity extends Assert< QuantityRef, QuantityDef > {}
-export class AssertUnit extends Assert< UnitRef, UnitDef > {}
-export class AssertConstant extends Assert< ConstantRef, ConstantDef > {}
+export class AssertPrefix extends Assert< PrefixRef, PrefixDef > {
+  protected override get registry () : IRegistry< PrefixRef > {
+    return this.ctx.services.registry.prefix;
+  }
+}
 
-export const createRegistryService = ( ctx: ServicesContext ) : IAssertService => ( {
+export class AssertQuantity extends Assert< QuantityRef, QuantityDef > {
+  protected override get registry () : IRegistry< QuantityRef > {
+    return this.ctx.services.registry.quantity;
+  }
+}
+
+export class AssertUnit extends Assert< UnitRef, UnitDef > {
+  protected override get registry () : IRegistry< UnitRef > {
+    return this.ctx.services.registry.unit;
+  }
+}
+
+export class AssertConstant extends Assert< ConstantRef, ConstantDef > {
+  protected override get registry () : IRegistry< ConstantRef > {
+    return this.ctx.services.registry.constant;
+  }
+}
+
+export const createAssertService = ( ctx: ServicesContext ) : IAssertService => ( {
   prefix: new AssertPrefix( ctx ),
   quantity: new AssertQuantity( ctx ),
   unit: new AssertUnit( ctx ),
