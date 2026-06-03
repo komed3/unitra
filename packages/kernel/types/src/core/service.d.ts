2e68a1083b54294a9e702c5f7870cad46ccc4861
komed3 <pk97@outlook.de>
2026-06-03 10:07:01 +0200

    type the IAssertService interface

--- packages/kernel/types/src/core/service.d.ts
+++ packages/kernel/types/src/core/service.d.ts
@@ -1,3 +1,8 @@
+import type { ConstantDef, ConstantRef } from '../def/constant';
+import type { PrefixDef, PrefixRef } from '../def/prefix';
+import type { QuantityDef, QuantityRef } from '../def/quantity';
+import type { UnitDef, UnitRef } from '../def/unit';
+
 export interface IAssert< Ref, Def > {
   isRef: ( value: unknown ) => value is Ref;
   isDef: ( value: unknown ) => value is Def;
@@ -5,7 +10,12 @@ export interface IAssert< Ref, Def > {
   assertDef: ( value: unknown ) => asserts value is Def;
 }
 
-export interface IAssertService {}
+export interface IAssertService {
+  prefix: IAssert< PrefixRef, PrefixDef >;
+  quantity: IAssert< QuantityRef, QuantityDef >;
+  unit: IAssert< UnitRef, UnitDef >;
+  constant: IAssert< ConstantRef, ConstantDef >;
+}
 
 export type ServicesContext = {
   services: {};
