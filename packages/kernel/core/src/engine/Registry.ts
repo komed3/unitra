5e53f6044e15827a45e66ee6a5289fb5023e012a
komed3 <pk97@outlook.de>
2026-06-03 19:30:19 +0200

    get stored map size

--- packages/kernel/core/src/engine/Registry.ts
+++ packages/kernel/core/src/engine/Registry.ts
@@ -3,6 +3,10 @@ import { AnyRef, RegistryDef } from '@unitra/types/registry';
 export class Registry< Ref extends AnyRef > {
   protected readonly store = new Map< Ref, RegistryDef< Ref > >();
 
+  public get size () : number {
+    return this.store.size;
+  }
+
   public get < R extends Ref > ( ref: R ) : RegistryDef< R > | undefined {
     return this.store.get( ref ) as RegistryDef< R > | undefined;
   }
