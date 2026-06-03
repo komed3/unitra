33f51616e280370a4383fc1c3da6f7cc483b950d
komed3 <pk97@outlook.de>
2026-06-03 02:09:29 +0200

    implement get/has/set methods

--- packages/kernel/core/src/registry/BaseRegistry.ts
+++ packages/kernel/core/src/registry/BaseRegistry.ts
@@ -2,4 +2,16 @@ import type { AnyRef, RegistryDef } from '@unitra/types/registry';
 
 export abstract class BaseRegistry< Ref extends AnyRef > {
   protected readonly store = new Map< Ref, RegistryDef< Ref > >();
+
+  public get < R extends Ref > ( ref: R ) : RegistryDef< R > | undefined {
+    return this.store.get( ref ) as RegistryDef< R > | undefined;
+  }
+
+  public has < R extends Ref > ( ref: R ) : boolean {
+    return this.store.has( ref );
+  }
+
+  public set < R extends Ref > ( ref: R, def: RegistryDef< R > ) : void {
+    this.store.set( ref, def );
+  }
 }
