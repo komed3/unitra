8a81645b2af84865fbc6857aebeed74e1f53df27
komed3 <pk97@outlook.de>
2026-06-04 00:32:35 +0200

    implement type guard for references

--- packages/kernel/core/src/service/AssertService.ts
+++ packages/kernel/core/src/service/AssertService.ts
@@ -1,5 +1,10 @@
+import type { AnyRef, IRegistry, RefOf, RegistryKey } from '@unitra/types/registry';
 import type { UnitraContext } from '@unitra/types/unitra';
 
 export class AssertService {
   constructor ( private readonly ctx: UnitraContext ) {}
+
+  public isRef < K extends RegistryKey, R extends AnyRef = RefOf< K > > ( key: K, value: unknown ) : value is R {
+    return typeof value === 'string' && ( this.ctx.registry( key ) as unknown as IRegistry< R > ).has( value as R );
+  }
 }
