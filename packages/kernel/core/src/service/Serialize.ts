6866d937680e5e0ad6899eb81fcff6404091489a
komed3 <pk97@outlook.de>
2026-06-04 12:17:43 +0200

    truncate code

--- packages/kernel/core/src/service/Serialize.ts
+++ packages/kernel/core/src/service/Serialize.ts
@@ -22,12 +22,9 @@ export class Serialize implements ISerialize {
   public fromUnitStruct ( struct: UnitStruct | CompoundStruct) : string {
     return this.fromReferenceState( { nodes: struct.map(
       ( node ) => 'factor' in node ? {
-        type: 'factor',
-        value: node.factor
+        type: 'factor', value: node.factor
       } : {
-        type: 'unit',
-        unit: node.unit,
-        exp: node.exp,
+        type: 'unit', unit: node.unit, exp: node.exp,
         modifier: 'modifier' in node ? node.modifier : undefined
       }
     ) } );
