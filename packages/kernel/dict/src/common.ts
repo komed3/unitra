a0673a17196c4b640c39f2b1ab24b381cb38cb20
komed3 <pk97@outlook.de>
2026-06-04 19:30:21 +0200

    add dict pkg back

---
+++ packages/kernel/dict/src/common.ts
@@ -0,0 +1,32 @@
+export enum Lang {
+  EN = 'en',
+  DE = 'de'
+}
+
+export enum System {
+  COMMON = 'common',
+  SI = 'si',
+  CGS = 'cgs',
+  GAUSSIAN = 'cgs_gaussian',
+  EMU = 'cgs_emu',
+  ESU = 'cgs_esu',
+  NATURAL = 'natural',
+  PLANCK = 'natural_planck',
+  ATOMIC = 'natural_atomic',
+  STONEY = 'natural_stoney',
+  IMPERIAL = 'imperial',
+  USC = 'usc',
+  ASTRO = 'astronomical'
+}
+
+export enum Format {
+  PLAIN = 'plain',
+  UNICODE = 'unicode',
+  LATEX = 'latex'
+}
+
+export enum SIType {
+  BASE = 'base',
+  DERIVED = 'derived',
+  ACCEPTED = 'accepted'
+}
