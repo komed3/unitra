667dcce4c56370ebd921214e7775d6581c9dbb0f
komed3 <pk97@outlook.de>
2026-06-04 20:03:31 +0200

    implement log level enum + logging types

---
+++ packages/kernel/dict/src/utils.ts
@@ -0,0 +1,7 @@
+export enum LogLevel {
+  DEBUG = 0,
+  LOG = 1,
+  WARN = 2,
+  ERROR = 3,
+  NONE = 4
+}
---
+++ packages/kernel/types/src/utils/logging.d.ts
@@ -0,0 +1,27 @@
+import type { LogLevel } from '@unitra/dict/utils';
+
+export type LogEntry = {
+  level: LogLevel;
+  source: string;
+  message: string;
+  timestamp: number;
+  isoTimestamp: string;
+  data?: unknown;
+};
+
+export interface ILogFormatter< T extends object > {
+  readonly config: Readonly< T >;
+  format: ( entry: LogEntry ) => string;
+}
+
+export interface ILogHandler< T extends object > {
+  readonly config: Readonly< T >;
+  write: ( entry: LogEntry ) => void;
+}
+
+export interface ILogger {
+  debug: ( message: string, data?: unknown ) => void;
+  log: ( message: string, data?: unknown ) => void;
+  warn: ( message: string, data?: unknown ) => void;
+  error: ( message: string, data?: unknown ) => void;
+}
