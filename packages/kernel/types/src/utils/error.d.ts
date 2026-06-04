1cd01f750c504b670f85644d9547e171f630e119
komed3 <pk97@outlook.de>
2026-06-04 22:35:06 +0200

    declare error types

---
+++ packages/kernel/types/src/utils/error.d.ts
@@ -0,0 +1,37 @@
+import type { ErrorCode } from '@unitra/dict/utils';
+
+export type UnitraErrorOptions< T extends object = never > =
+  [ T ] extends [ never ]
+    ? { cause?: unknown }
+    : { data: T, cause?: unknown };
+
+export type SerializedError = {
+  name: string;
+  code?: ErrorCode;
+  message: string;
+  summary?: string;
+  stack?: string;
+  data?: unknown;
+  cause?: SerializedError;
+};
+
+export type ErrorFormatterConfig = {
+  showCode?: boolean;
+  showSummary?: boolean;
+  showData?: boolean;
+  showStack?: boolean;
+  showCauses?: boolean;
+  indent?: string;
+};
+
+export interface IUnitraError< C extends ErrorCode = ErrorCode, T extends object = never > extends Error {
+  readonly code?: C;
+  readonly data?: T;
+  readonly cause?: unknown;
+  readonly type: string;
+  readonly summary: string | undefined;
+  toString: () => string;
+  serialize: () => SerializedError;
+  format: ( options?: ErrorFormatterConfig ) => string;
+  log: () => void;
+}
