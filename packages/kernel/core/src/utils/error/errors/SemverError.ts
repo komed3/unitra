5877cd98f63682a8ce508d6aaca03838c40b6ce0
komed3 <pk97@outlook.de>
2026-06-05 01:20:49 +0200

    min fix

--- packages/kernel/core/src/utils/error/errors/SemverError.ts
+++ packages/kernel/core/src/utils/error/errors/SemverError.ts
@@ -1,14 +1,6 @@
 import { ErrorCode } from '@unitra/dict/utils';
-import type { SemverVersion } from '@unitra/types/utils/semver';
 import { UnitraError } from '../UnitraError';
 
-export type SemverErrorData = {
-  version: SemverVersion;
-  semver: string;
-  tag: string;
-  parts: string[];
-};
-
-export class SemverError extends UnitraError< ErrorCode.SEMVER_ERROR, SemverErrorData > {
+export class SemverError extends UnitraError< ErrorCode.SEMVER_ERROR > {
   public override readonly code = ErrorCode.SEMVER_ERROR;
 }
