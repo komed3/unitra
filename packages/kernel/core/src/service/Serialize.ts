8f86358fd862178d38a1a6b24051d52760b2539c
komed3 <pk97@outlook.de>
2026-06-04 12:01:49 +0200

    prep. serializer class

--- packages/kernel/core/src/service/Serialize.ts
+++ packages/kernel/core/src/service/Serialize.ts
@@ -1,5 +1,11 @@
+import type { ReferenceState } from '@unitra/types/node';
 import type { ISerialize } from '@unitra/types/service';
+import type { CompoundStruct, UnitStruct } from '@unitra/types/unit';
 
 export class Serialize implements ISerialize {
   constructor () {}
+
+  public fromReference ( state: ReferenceState ) : string {}
+
+  public fromUnitStruct ( struct: UnitStruct | CompoundStruct) : string {}
 }
