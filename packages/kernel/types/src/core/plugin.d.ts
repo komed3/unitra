99060f02cd7b6f4df50ce5090a6cca32c2f104ac
komed3 <pk97@outlook.de>
2026-06-04 23:34:27 +0200

    declare plugin definition type

--- packages/kernel/types/src/core/plugin.d.ts
+++ packages/kernel/types/src/core/plugin.d.ts
@@ -2,7 +2,7 @@ import type { ConstantMap } from '../def/constant';
 import type { PrefixMap } from '../def/prefix';
 import type { QuantityMap } from '../def/quantity';
 import type { UnitMap } from '../def/unit';
-import type { SemverRange } from '../utils/semver';
+import type { SemverRange, SemverVersion } from '../utils/semver';
 
 export type DependencyMap = Readonly< Record< string, SemverRange > >;
 
@@ -17,4 +17,12 @@ export type PluginContributions = {
   quantities?: ReadonlyArray< QuantityMap >;
   units?: ReadonlyArray< UnitMap >;
   constants?: ReadonlyArray< ConstantMap >;
+};
+
+export type PluginDefinition = {
+  readonly id: string;
+  readonly version: SemverVersion;
+  meta: PluginMeta;
+  dependencies?: DependencyMap;
+  contributions?: PluginContributions;
 };
