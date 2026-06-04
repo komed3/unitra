08d6500aacc7ba5b3fd93a9e2837d75255f2800e
komed3 <pk97@outlook.de>
2026-06-04 23:52:15 +0200

    implement private methods for semver tilde / caret checks

--- packages/kernel/core/src/utils/semver.ts
+++ packages/kernel/core/src/utils/semver.ts
@@ -1,3 +1,5 @@
+import type { ParsedSemverVersion } from '@unitra/types/utils/semver';
+
 export class Semver {
   private static readonly OPMATCH = /^(\^|~|>=|<=|>|<|=)/;
 
@@ -8,4 +10,16 @@ export class Semver {
     '<': ( cmp: number ) => cmp < 0,
     '<=': ( cmp: number ) => cmp <= 0
   } as const;
+
+  private static satisfiesTilde ( version: ParsedSemverVersion, range: ParsedSemverVersion, cmp: number ) : boolean {
+    return cmp >= 0 && version[ 0 ] === range[ 0 ] && version[ 1 ] === range[ 1 ];
+  }
+
+  private static satisfiesCaret ( version: ParsedSemverVersion, range: ParsedSemverVersion, cmp: number ) : boolean {
+    return cmp >= 0 && (
+      ( range[ 0 ] > 0 && version[ 0 ] === range[ 0 ] ) ||
+      ( range[ 1 ] > 0 && version[ 0 ] === 0 && version[ 1 ] === range[ 1 ] ) ||
+      ( version[ 0 ] === 0 && version[ 1 ] === 0 && version[ 2 ] === range[ 2 ] )
+    );
+  }
 }
