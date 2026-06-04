948ce4d506e1015618417a5d85ed706b80787085
komed3 <pk97@outlook.de>
2026-06-04 23:51:24 +0200

    Create semver.ts

---
+++ packages/kernel/core/src/utils/semver.ts
@@ -0,0 +1,11 @@
+export class Semver {
+  private static readonly OPMATCH = /^(\^|~|>=|<=|>|<|=)/;
+
+  private static readonly OPERATORS = {
+    '=': ( cmp: number ) => cmp === 0,
+    '>': ( cmp: number ) => cmp > 0,
+    '>=': ( cmp: number ) => cmp >= 0,
+    '<': ( cmp: number ) => cmp < 0,
+    '<=': ( cmp: number ) => cmp <= 0
+  } as const;
+}
