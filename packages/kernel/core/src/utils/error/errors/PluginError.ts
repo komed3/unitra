f38b8c84fa55abb9ce649aaf6c39d124043a9fd3
komed3 <pk97@outlook.de>
2026-06-05 01:39:49 +0200

    Update PluginError.ts

--- packages/kernel/core/src/utils/error/errors/PluginError.ts
+++ packages/kernel/core/src/utils/error/errors/PluginError.ts
@@ -3,4 +3,18 @@ import { UnitraError } from '../UnitraError';
 
 export class PluginError extends UnitraError< ErrorCode.PLUGIN_ERROR > {
   public override readonly code = ErrorCode.PLUGIN_ERROR;
+
+  public override get summary () : string {
+    const parts: string[] = [];
+
+    if ( this.context ) {
+      const { missing, conflicts, cycles } = this.context;
+
+      if ( missing.length ) parts.push( `Missing dependencies:\n└─ ${ missing.join( '\n└─ ' ) }` );
+      if ( conflicts.length ) parts.push( `Version conflicts:\n└─ ${ conflicts.join( '\n└─ ' ) }` );
+      if ( cycles.length ) parts.push( `Dependency cycles:\n└─ ${ cycles.join( '\n└─ ' ) }` );
+    }
+
+    return parts.join( '\n\n' );
+  }
 }
