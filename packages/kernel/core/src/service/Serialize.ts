330f1cc13540cc9845182bf7bd106b975974e4e0
komed3 <pk97@outlook.de>
2026-06-04 14:27:56 +0200

    rework serializer class

--- packages/kernel/core/src/service/Serialize.ts
+++ packages/kernel/core/src/service/Serialize.ts
@@ -1,34 +1,39 @@
-import type { ReferenceState } from '@unitra/types/node';
+import type { NodeMap, ReferenceState, SerializedNode, SerializerMap } from '@unitra/types/node';
 import type { ISerialize } from '@unitra/types/service';
 import type { CompoundStruct, UnitStruct } from '@unitra/types/unit';
 import type { UnitraContext } from '@unitra/types/unitra';
 
 export class Serialize implements ISerialize {
+  private static readonly map: SerializerMap = {
+    factor: ( node ) => ( { order: 0, value: `#${ node.value }` } ),
+    constant: (node) => ( { order: 1, value: `@${ node.constant }^${ node.exp }` } ),
+    unit: ( node ) => ( { order: 2, value: `${ node.prefix ? `${ node.prefix }:` : '' }${ node.unit }^${ node.exp }` } )
+  };
+
+  private static serializeNode < K extends keyof NodeMap > ( type: K, node: NodeMap[ K ] ) : SerializedNode {
+    return this.map[ type ]( node );
+  }
+
   constructor ( private readonly ctx: UnitraContext ) {}
 
   public fromReferenceState ( state: ReferenceState ) : string {
-    return this.ctx.core.hook.run( 'core.service.serialize', { state }, state.nodes
-      .map( ( node ) => {
-        switch ( node.type ) {
-          case 'factor': return { order: 0, value: `#${ node.value }` };
-          case 'constant': return { order: 1, value: `@${ node.constant }^${ node.exp }` };
-          case 'unit': return { order: 2, value: `${ node.prefix ? `${ node.prefix }:` : '' }${ node.unit }^${ node.exp }` };
-        }
-      } )
-      .sort( ( a, b ) => a.order - b.order || a.value.localeCompare( b.value ) )
-      .map( ( node ) => node.value )
-      .join( '*' )
-    );
+    return this.ctx.core.hook.run( 'core.service.serialize', { state }, `$${ this.ctx.V }::${
+      state.nodes
+        .map( ( node ) => Serialize.serializeNode( node.type, node ) )
+        .sort( ( a, b ) => a.order - b.order || a.value.localeCompare( b.value ) )
+        .map( ( n ) => n.value )
+        .join( '*' )
+    }` );
   }
 
   public fromUnitStruct ( struct: UnitStruct | CompoundStruct ) : string {
     return this.fromReferenceState( { nodes: struct.map(
-      ( node ) => 'factor' in node ? {
-        type: 'factor', value: node.factor
-      } : {
-        type: 'unit', unit: node.unit, exp: node.exp,
-        modifier: 'modifier' in node ? node.modifier : undefined
-      }
+      ( node ) => 'factor' in node
+        ? { type: 'factor', value: node.factor }
+        : {
+          type: 'unit', unit: node.unit, exp: node.exp,
+          prefix: 'prefix' in node ? node.prefix : undefined
+        }
     ) } );
   }
 }
