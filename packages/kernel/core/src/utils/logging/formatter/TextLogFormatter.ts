fcec43bb43f2b61b814740db28e23a61183d49ca
komed3 <pk97@outlook.de>
2026-06-04 20:49:53 +0200

    implement log formatter

---
+++ packages/kernel/core/src/utils/logging/formatter/TextLogFormatter.ts
@@ -0,0 +1,25 @@
+import { LogLevel } from '@unitra/dict/utils';
+import type { LogEntry } from '@unitra/types/utils/logging';
+import { safeJsonStringify } from '../../json';
+import { LogFormatter } from './LogFormatter';
+
+export type TextLogFormatterConfig = {
+  format?: string;
+  showData?: boolean;
+};
+
+export class TextLogFormatter extends LogFormatter< TextLogFormatterConfig > {
+  public format ( entry: LogEntry ) : string {
+    const out: string[] = [
+      `[${ entry.isoTimestamp }]`,
+      `[${ LogLevel[ entry.level ] }]`,
+      `[${ entry.source }]`,
+      entry.message
+    ];
+
+    if ( this.config.showData !== false && entry.data !== undefined )
+      out.push( safeJsonStringify( entry.data ) );
+
+    return out.join( ' ' );
+  }
+}
