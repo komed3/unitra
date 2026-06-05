170ab7d524a2ed0a60b5928bdc68a2cc284f44aa
komed3 <pk97@outlook.de>
2026-06-05 12:12:09 +0200

    mount resolve service

--- packages/kernel/core/src/service/index.ts
+++ packages/kernel/core/src/service/index.ts
@@ -1,6 +1,9 @@
 import type { ServiceAccessor, ServiceContainer, ServiceFactoryMap, ServiceInstanceMap } from '@unitra/types/core/service';
 import type { UnitraContext } from '@unitra/types/core/unitra';
 import { Assert } from './Assert';
+import { Resolve } from './Resolve';
+
+export { Assert, Resolve };
 
 export const createServiceAccessor = (
   ctx: UnitraContext,
@@ -8,14 +11,12 @@ export const createServiceAccessor = (
 ) : ServiceAccessor => {
   const cache: Partial< ServiceInstanceMap > = {};
   const defaults: ServiceFactoryMap = {
-    assert: ( ctx ) => new Assert( ctx )
+    assert: ( ctx ) => new Assert( ctx ),
+    resolve: ( ctx ) => new Resolve( ctx )
   };
 
   return () : ServiceContainer => ( {
-    assert: () => cache.assert ??= ( factories?.assert ?? defaults.assert )( ctx )
+    assert: () => cache.assert ??= ( factories?.assert ?? defaults.assert )( ctx ),
+    resolve: () => cache.resolve ??= ( factories?.resolve ?? defaults.resolve )( ctx )
   } );
-};
-
-export {
-  Assert
 };
