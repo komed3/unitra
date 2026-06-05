3bdf6ec44788c0301d6d3ce021d7a874384e0793
komed3 <pk97@outlook.de>
2026-06-05 23:59:12 +0200

    min fix

--- packages/kernel/core/src/service/Serialize.ts
+++ packages/kernel/core/src/service/Serialize.ts
@@ -1,5 +1,7 @@
+import type { SerializerMap } from '@unitra/types/core/node';
+
 export class Serialize {
-  private static readonly map = {
+  private static readonly map: SerializerMap = {
     factor: ( node ) => ( { order: 0, value: `#${ node.value }` } ),
     constant: ( node ) => ( { order: 1, value: `@${ node.constant }^${ node.exp }` } ),
     unit: ( node ) => ( { order: 2, value: `${ node.prefix ? `${ node.prefix }:` : '' }${ node.unit }^${ node.exp }` } )
