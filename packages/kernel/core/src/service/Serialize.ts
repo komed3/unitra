89cc8e13f1a0ab6ddef4b685c27d28ff65f1fed1
komed3 <pk97@outlook.de>
2026-06-05 23:59:41 +0200

    Update Serialize.ts

--- packages/kernel/core/src/service/Serialize.ts
+++ packages/kernel/core/src/service/Serialize.ts
@@ -1,4 +1,5 @@
-import type { SerializerMap } from '@unitra/types/core/node';
+import type { NodeMap, SerializedNode, SerializerMap } from '@unitra/types/core/node';
+import type { UnitraContext } from '@unitra/types/core/unitra';
 
 export class Serialize {
   private static readonly map: SerializerMap = {
@@ -6,4 +7,10 @@ export class Serialize {
     constant: ( node ) => ( { order: 1, value: `@${ node.constant }^${ node.exp }` } ),
     unit: ( node ) => ( { order: 2, value: `${ node.prefix ? `${ node.prefix }:` : '' }${ node.unit }^${ node.exp }` } )
   } as const;
+
+  private static serializeNode < K extends keyof NodeMap > ( type: K, node: NodeMap[ K ] ) : SerializedNode {
+    return this.map[ type ]( node );
+  }
+
+  constructor ( private readonly ctx: UnitraContext ) {}
 }
