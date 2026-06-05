4d85640f6c4a8ed68e213a5fca134cc010cab6da
komed3 <pk97@outlook.de>
2026-06-05 14:45:04 +0200

    declare hook helper types + handler

--- packages/kernel/types/src/core/hook.d.ts
+++ packages/kernel/types/src/core/hook.d.ts
@@ -1,6 +1,22 @@
+import type { UnitraContext } from './unitra';
+
 export interface HookRegistry {
   'core.service.serialize': {
     ctx: {};
     value: string;
   };
 }
+
+export type HookId = keyof HookRegistry;
+export type HookSpec< K extends HookId > = HookRegistry[ K ];
+export type HookCtx< K extends HookId > = HookSpec< K >[ 'ctx' ];
+
+export type HookValue< K extends HookId > =
+  HookSpec< K > extends { value: infer V }
+    ? V
+    : void;
+
+export type HookHandler< K extends HookId > =
+  HookSpec< K > extends { value: infer V }
+    ? ( ctx: UnitraContext, hookCtx: HookCtx< K >, value: V ) => V
+    : ( ctx: UnitraContext, hookCtx: HookCtx< K > ) => void;
