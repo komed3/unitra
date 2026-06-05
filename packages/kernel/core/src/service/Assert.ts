1b38aa5ee241e9734e206e16f2c01386c05fde07
komed3 <pk97@outlook.de>
2026-06-05 10:19:54 +0200

    test if value is ref

--- packages/kernel/core/src/service/Assert.ts
+++ packages/kernel/core/src/service/Assert.ts
@@ -1,5 +1,11 @@
+import type { RefOf, RegistryKey } from '@unitra/types/core/registry';
 import type { UnitraContext } from '@unitra/types/core/unitra';
+import { getTypedRegistry } from '../engine/Registry';
 
 export class Assert {
   constructor ( private readonly ctx: UnitraContext ) {}
+
+  public isRef < K extends RegistryKey > ( key: K, value: unknown ) : value is RefOf< K > {
+    return typeof value === 'string' && getTypedRegistry( this.ctx, key ).has( value as RefOf< K > );
+  }
 }
