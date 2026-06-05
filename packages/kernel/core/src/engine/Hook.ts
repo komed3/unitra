2ef15ce7d45e4947decf7e22f569a3c9b3eaeeb5
komed3 <pk97@outlook.de>
2026-06-05 17:42:32 +0200

    prep. hook storage

--- packages/kernel/core/src/engine/Hook.ts
+++ packages/kernel/core/src/engine/Hook.ts
@@ -1,5 +1,10 @@
 import type { UnitraContext } from '@unitra/types/core/unitra';
+import { Logging } from '../utils';
 
 export class Hook {
+  private static readonly log = Logging.createSource( 'hook' );
+  private readonly hooks = new Map();
+  private readonly cache = new Map();
+
   constructor ( private readonly ctx: UnitraContext ) {}
 }
