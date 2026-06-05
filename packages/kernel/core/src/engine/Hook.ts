ff1b4be49dad53f7113f22b2074985e610bf0a20
komed3 <pk97@outlook.de>
2026-06-05 17:47:22 +0200

    implement hook storage / cache classes

--- packages/kernel/core/src/engine/Hook.ts
+++ packages/kernel/core/src/engine/Hook.ts
@@ -1,10 +1,37 @@
+import { HookDef, HookHandler, HookId, HookPipeline } from '@unitra/types/core/hook';
 import type { UnitraContext } from '@unitra/types/core/unitra';
 import { Logging } from '../utils';
 
+class HookStorage extends Map {
+  public override set < K extends HookId > ( id: K, hooks: HookDef< K >[] ) : this {
+    return super.set( id, hooks );
+  }
+
+  public override get < K extends HookId > ( id: K ) : HookDef< K >[] | undefined {
+    return super.get( id );
+  }
+}
+
+class HookCache extends Map {
+  public override set < K extends HookId > ( id: K, fn: HookPipeline< K > ) : this {
+    return super.set( id, fn );
+  }
+
+  public override get < K extends HookId > ( id: K ) : HookPipeline< K > | undefined {
+    return super.get( id );
+  }
+}
+
 export class Hook {
   private static readonly log = Logging.createSource( 'hook' );
-  private readonly hooks = new Map();
-  private readonly cache = new Map();
+  private readonly hooks = new HookStorage();
+  private readonly cache = new HookCache();
 
   constructor ( private readonly ctx: UnitraContext ) {}
+
+  public add < K extends HookId > ( id: K, handler: HookHandler< K >, priority?: number ) : void {
+    const list = this.hooks.get( id ) || [];
+    list.push( { handler, priority } );
+    this.hooks.set( id, list );
+  }
 }
