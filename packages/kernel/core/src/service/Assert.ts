e80ba93fa82089ab1bc62f5f1b85d6a6e3c7daa5
komed3 <pk97@outlook.de>
2026-06-05 10:27:33 +0200

    assert ref / def

--- packages/kernel/core/src/service/Assert.ts
+++ packages/kernel/core/src/service/Assert.ts
@@ -1,6 +1,7 @@
 import type { DefOf, RefOf, RegistryKey } from '@unitra/types/core/registry';
 import type { UnitraContext } from '@unitra/types/core/unitra';
 import { getTypedRegistry } from '../engine/Registry';
+import { AssertError, safeJsonStringify } from '../utils';
 
 export class Assert {
   constructor ( private readonly ctx: UnitraContext ) {}
@@ -11,5 +12,21 @@ export class Assert {
 
   public isDef < K extends RegistryKey > ( key: K, value: unknown ) : value is DefOf< K > {
     return typeof value === 'object' && value !== null && 'id' in value && this.isRef( key, value.id );
+  }
+
+  public assertRef < K extends RegistryKey > ( key: K, value: unknown ) : asserts value is RefOf< K > {
+    if ( ! this.isRef( key, value ) )
+      throw new AssertError(
+        `expected a ${ key } reference, but got ${ safeJsonStringify( value ) }`,
+        { context: { key, value } }
+      );
+  }
+
+  public assertDef < K extends RegistryKey > ( key: K, value: unknown ) : asserts value is DefOf< K > {
+    if ( ! this.isDef( key, value ) )
+      throw new AssertError(
+        `expected a ${ key } definition, but got ${ safeJsonStringify( value ) }`,
+        { context: { key, value } }
+      );
   }
 }
