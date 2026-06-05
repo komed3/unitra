97568671383bc4826bb7d9e312c19966791d6e21
komed3 <pk97@outlook.de>
2026-06-06 01:04:26 +0200

    create next() helper method

--- packages/kernel/core/src/service/Factory.ts
+++ packages/kernel/core/src/service/Factory.ts
@@ -1,4 +1,4 @@
-import type { ReferenceState } from '@unitra/types/core/node';
+import type { Node, ReferenceState } from '@unitra/types/core/node';
 import type { UnitraContext } from '@unitra/types/core/unitra';
 
 export class UnitFactory {
@@ -6,4 +6,8 @@ export class UnitFactory {
     private readonly ctx: UnitraContext,
     private readonly state: ReferenceState = { nodes: [] }
   ) {}
+
+  private next ( nodes: Node[] = [] ) : UnitFactory {
+    return new UnitFactory( this.ctx, { nodes: [ ...this.state.nodes, ...nodes ] } );
+  }
 }
