8a5612b462775dad6c949393abf0b6c9c15e2dab
komed3 <pk97@outlook.de>
2026-06-06 18:16:00 +0200

    update files

--- packages/kernel/core/src/core/index.ts
+++ packages/kernel/core/src/core/index.ts
@@ -2,9 +2,11 @@ import type { CoreContainer, UnitraContext } from '@unitra/types/core/unitra';
 import { createContainer, Logging } from '../utils';
 import { Hook } from './Hook';
 
-export const createCoreContainer = ( ctx: UnitraContext ) : CoreContainer => (
-  createContainer( ctx, {
-    log: () => Logging.createSource( 'global' ),
-    hook: ctx => new Hook( ctx )
-  } )
-);
+export const createCoreContainer = ( ctx: UnitraContext ) : CoreContainer =>
+  createContainer(
+    ctx,
+    {
+      log: () => Logging.createSource( 'global' ),
+      hook: ctx => new Hook( ctx )
+    }
+  );
--- packages/kernel/core/src/factory/index.ts
+++ packages/kernel/core/src/factory/index.ts
@@ -6,8 +6,12 @@ import { UnitFactory } from './UnitFactory';
 export const createFactoryContainer = (
   ctx: UnitraContext,
   factories?: Partial< FactoryFactoryMap >
-) : FactoryContainer => (
-  createContainer( ctx, {
-    unit: ctx => new UnitFactory( ctx )
-  }, factories, { cache: false } )
-);
+) : FactoryContainer =>
+  createContainer(
+    ctx,
+    {
+      unit: ctx => new UnitFactory( ctx )
+    },
+    factories,
+    { cache: false }
+  );
