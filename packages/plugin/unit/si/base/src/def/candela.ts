ffc659ad6e5474e812a8662e73e66cb4341a9bb9
komed3 <pk97@outlook.de>
2026-06-06 17:30:21 +0200

    add the 7 base units for SI system

---
+++ packages/plugin/unit/si/base/src/def/candela.ts
@@ -0,0 +1,50 @@
+import { Format, Lang, SIType, System } from '@unitra/dict/common';
+import { UnitStatus, UnitType } from '@unitra/dict/unit';
+import { LuminousIntensityDim } from '@unitra/dim/base';
+import { luminousIntensity } from '@unitra/plugin-quantity-base/def/luminousIntensity';
+import type { DerivedUnitDef, UnitRef } from '@unitra/types/def/unit';
+
+export const candela = 'cd' as UnitRef< typeof LuminousIntensityDim, UnitType.NAMED, 'cd' >;
+
+export default ( {
+  type: UnitType.NAMED,
+  id: candela,
+  dim: LuminousIntensityDim,
+  structure: [],
+  conversion: {
+    type: 'identity'
+  },
+  prefixable: true,
+  aliases: [
+    'candela',
+    'candelas'
+  ],
+  context: {
+    system: [
+      System.SI
+    ],
+    status: UnitStatus.ACTIVE,
+    si: SIType.BASE,
+    quantities: [
+      luminousIntensity
+    ]
+  },
+  meta: {
+    symbol: [ {
+      id: 'cd',
+      canonical: true,
+      format: {
+        [ Format.PLAIN ]: 'cd',
+        [ Format.LATEX ]: '\\mathrm{cd}'
+      }
+    } ],
+    name: {
+      [ Lang.EN ]: [ 'candela', 'candelas' ],
+      [ Lang.DE ]: [ 'Candela' ]
+    },
+    description: {
+      [ Lang.EN ]: 'SI unit of luminous intensity',
+      [ Lang.DE ]: 'SI-Basiseinheit der Lichtstärke'
+    }
+  }
+} ) as const satisfies DerivedUnitDef< typeof candela >;
---
+++ packages/plugin/unit/si/base/src/def/second.ts
@@ -0,0 +1,52 @@
+import { Format, Lang, SIType, System } from '@unitra/dict/common';
+import { UnitStatus, UnitType } from '@unitra/dict/unit';
+import { TimeDim } from '@unitra/dim/base';
+import { time } from '@unitra/plugin-quantity-base/def/time';
+import type { DerivedUnitDef, UnitRef } from '@unitra/types/def/unit';
+
+export const second = 's' as UnitRef< typeof TimeDim, UnitType.NAMED, 's' >;
+
+export default ( {
+  type: UnitType.NAMED,
+  id: second,
+  dim: TimeDim,
+  structure: [],
+  conversion: {
+    type: 'identity'
+  },
+  prefixable: true,
+  aliases: [
+    'second',
+    'seconds',
+    'sec'
+  ],
+  context: {
+    system: [
+      System.COMMON,
+      System.SI
+    ],
+    status: UnitStatus.ACTIVE,
+    si: SIType.BASE,
+    quantities: [
+      time
+    ]
+  },
+  meta: {
+    symbol: [ {
+      id: 's',
+      canonical: true,
+      format: {
+        [ Format.PLAIN ]: 's',
+        [ Format.LATEX ]: '\\mathrm{s}'
+      }
+    } ],
+    name: {
+      [ Lang.EN ]: [ 'second', 'seconds' ],
+      [ Lang.DE ]: [ 'Sekunde', 'Sekunden' ]
+    },
+    description: {
+      [ Lang.EN ]: 'SI unit of time',
+      [ Lang.DE ]: 'SI-Basiseinheit der Zeit'
+    }
+  }
+} ) as const satisfies DerivedUnitDef< typeof second >;
