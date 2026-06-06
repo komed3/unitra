24f96a1b957e61dd01707371487e59392e52195b
komed3 <pk97@outlook.de>
2026-06-06 17:20:38 +0200

    declare SI base quantities map

---
+++ packages/plugin/quantity/base/src/index.ts
@@ -0,0 +1,19 @@
+import type { QuantityMap } from '@unitra/types/def/quantity';
+
+import amountOfSubstanceDef, { amountOfSubstance } from './def/amountOfSubstance';
+import electricCurrentDef, { electricCurrent } from './def/electricCurrent';
+import lengthDef, { length } from './def/length';
+import luminousIntensityDef, { luminousIntensity } from './def/luminousIntensity';
+import massDef, { mass } from './def/mass';
+import temperatureDef, { temperature } from './def/temperature';
+import timeDef, { time } from './def/time';
+
+export default ( {
+  [ amountOfSubstance ]: amountOfSubstanceDef,
+  [ electricCurrent ]: electricCurrentDef,
+  [ length ]: lengthDef,
+  [ luminousIntensity ]: luminousIntensityDef,
+  [ mass ]: massDef,
+  [ temperature ]: temperatureDef,
+  [ time ]: timeDef
+} ) as const satisfies QuantityMap;
