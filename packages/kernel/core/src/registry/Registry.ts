b09609829743ed46faac3145b5a1956acf913d2d
komed3 <pk97@outlook.de>
2026-06-06 09:38:20 +0200

    add get/has/set

--- packages/kernel/core/src/registry/Registry.ts
+++ packages/kernel/core/src/registry/Registry.ts
@@ -1,6 +1,25 @@
-import type { AnyRef, IRegistry } from '@unitra/types/core/registry';
+import type { AnyRef, IRegistry, RegistryDef } from '@unitra/types/core/registry';
+import type { UnitraContext } from '@unitra/types/core/unitra';
 import { RegistryStorage } from './RegistryStorage';
 
 export class Registry< Ref extends AnyRef > implements IRegistry< Ref > {
   private readonly items = new RegistryStorage< Ref >();
+
+  constructor ( private readonly ctx: UnitraContext ) {}
+
+  public get size () : number {
+    return this.items.size;
+  }
+
+  public get < R extends Ref > ( ref: R ) : RegistryDef< R > | undefined {
+    return this.items.get( ref );
+  }
+
+  public has ( ref: Ref ) : boolean {
+    return this.items.has( ref );
+  }
+
+  public set < R extends Ref > ( ref: R, def: RegistryDef< R > ) : void {
+    this.items.set( ref, def );
+  }
 }
