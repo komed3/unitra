e57062b8c43fc5836f82364abfa0bb4129c275b8
komed3 <pk97@outlook.de>
2026-06-06 18:10:18 +0200

    update container fns

--- packages/kernel/core/src/core/index.ts
+++ packages/kernel/core/src/core/index.ts
@@ -1,20 +1,10 @@
-import type { CoreContainer, CoreFactoryMap, CoreInstanceMap, UnitraContext } from '@unitra/types/core/unitra';
-import { Logging } from '../utils';
+import type { CoreContainer, UnitraContext } from '@unitra/types/core/unitra';
+import { createContainer, Logging } from '../utils';
 import { Hook } from './Hook';
 
-export const createCoreContainer = ( ctx: UnitraContext ) : CoreContainer => {
-  const cache: Partial< CoreInstanceMap > = {};
-
-  const defaults: CoreFactoryMap = {
+export const createCoreContainer = ( ctx: UnitraContext ) : CoreContainer => (
+  createContainer( ctx, {
     log: () => Logging.createSource( 'global' ),
-    hook: ( ctx ) => new Hook( ctx )
-  };
-
-  const get = < K extends keyof CoreInstanceMap > ( key: K ) : CoreInstanceMap[ K ] =>
-    cache[ key ] ??= defaults[ key ]( ctx );
-
-  return Object.freeze( {
-    log: () => get( 'log' ),
-    hook: () => get( 'hook' )
-  } );
-};
+    hook: ctx => new Hook( ctx )
+  } )
+);
