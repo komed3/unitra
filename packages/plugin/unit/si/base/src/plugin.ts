2463b90dee02fe99885bc36e76d9b2b0be63f577
komed3 <pk97@outlook.de>
2026-06-06 17:33:38 +0200

    declare SI unit pack plugin def

---
+++ packages/plugin/unit/si/base/src/plugin.ts
@@ -0,0 +1,23 @@
+import type { PluginDefinition } from '@unitra/types/core/plugin';
+import siBaseUnitRegistry from './';
+
+export default ( {
+  id: '@unitra/plugin-unit-si-base',
+  version: '0.0.1',
+  meta: {
+    name: 'SI Base Units',
+    description: 'Unitra plugin for SI base units',
+    tags: [ 'si', 'units' ],
+    license: 'MIT',
+    author: 'Paul Köhler (komed3)',
+    repo: 'https://github.com/komed3/unitra/packages/plugin/unit/si/base'
+  },
+  dependencies: {
+    '@unitra/plugin-quantity-base': '^0.0.1'
+  },
+  contributions: {
+    units: [
+      siBaseUnitRegistry
+    ]
+  }
+} ) as const satisfies PluginDefinition;
