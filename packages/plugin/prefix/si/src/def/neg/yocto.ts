3e53f943a1597540d772663c9cdf47abc8ff4b76
komed3 <pk97@outlook.de>
2026-06-06 16:59:29 +0200

    declare SI prefixes

---
+++ packages/plugin/prefix/si/src/def/neg/yocto.ts
@@ -0,0 +1,30 @@
+import { Format, Lang } from '@unitra/dict/common';
+import type { DerivedPrefixDef, PrefixRef } from '@unitra/types/def/prefix';
+
+export const yocto = 'y' as PrefixRef< 'y' >;
+
+export default ( {
+  id: yocto,
+  factor: 1e-24,
+  aliases: [
+    'yocto'
+  ],
+  meta: {
+    symbol: [ {
+      id: 'y',
+      canonical: true,
+      format: {
+        [ Format.PLAIN ]: 'y',
+        [ Format.LATEX ]: '\\mathrm{y}'
+      }
+    } ],
+    name: {
+      [ Lang.EN ]: 'yocto',
+      [ Lang.DE ]: 'Jokto'
+    },
+    description: {
+      [ Lang.EN ]: 'unit prefix with the factor 1e-24',
+      [ Lang.DE ]: 'Einheitenpräfix mit dem Faktor 1e-24'
+    }
+  }
+} ) as const satisfies DerivedPrefixDef< typeof yocto >;
