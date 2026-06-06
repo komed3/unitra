1c033edf2b0eec0b9df4a0839f8b053d06a7d1b9
komed3 <pk97@outlook.de>
2026-06-06 18:14:11 +0200

    update service container

--- packages/kernel/core/src/service/index.ts
+++ packages/kernel/core/src/service/index.ts
@@ -1,5 +1,6 @@
-import type { ServiceContainer, ServiceFactoryMap, ServiceInstanceMap, ServiceKey } from '@unitra/types/core/service';
+import type { ServiceContainer, ServiceFactoryMap } from '@unitra/types/core/service';
 import type { UnitraContext } from '@unitra/types/core/unitra';
+import { createContainer } from '../utils';
 import { Assert } from './Assert';
 import { Resolve } from './Resolve';
 import { Serialize } from './Serialize';
@@ -7,21 +8,13 @@ import { Serialize } from './Serialize';
 export const createServiceContainer = (
   ctx: UnitraContext,
   factories?: Partial< ServiceFactoryMap >
-) : ServiceContainer => {
-  const cache: Partial< ServiceInstanceMap > = {};
-
-  const defaults: ServiceFactoryMap = {
-    assert: ( ctx ) => new Assert( ctx ),
-    resolve: ( ctx ) => new Resolve( ctx ),
-    serialize: ( ctx ) => new Serialize( ctx )
-  };
-
-  const get = < K extends ServiceKey > ( key: K ) : ServiceInstanceMap[ K ] =>
-    cache[ key ] ??= ( factories?.[ key ] ?? defaults[ key ] )( ctx );
-
-  return Object.freeze( {
-    assert: () => get( 'assert' ),
-    resolve: () => get( 'resolve' ),
-    serialize: () => get( 'serialize' )
-  } );
-};
+) : ServiceContainer =>
+  createContainer(
+    ctx,
+    {
+      assert: ctx => new Assert( ctx ),
+      resolve: ctx => new Resolve( ctx ),
+      serialize: ctx => new Serialize( ctx )
+    },
+    factories
+  );
