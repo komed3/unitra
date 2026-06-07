09f08c6e5389a0d2145026fa7fee15f5c1c23e82
komed3 <pk97@outlook.de>
2026-06-08 00:39:24 +0200

    mount hook accessor to unitra ctx

--- packages/kernel/types/src/core/unitra.d.ts
+++ packages/kernel/types/src/core/unitra.d.ts
@@ -1,9 +1,11 @@
 import type { FactoryContainer } from './factory';
+import type { HookAccessor } from './hook';
 import type { RegistryContainer } from './registry';
 import type { ServiceContainer } from './service';
 
 export type UnitraContext = {
   readonly VERSION: 1;
+  hook: HookAccessor;
   registry: RegistryContainer;
   service: ServiceContainer;
   factory: FactoryContainer;
