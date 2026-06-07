97d2763fcdcfe453f4fbb9f0a50eaca2a9b96307
komed3 <pk97@outlook.de>
2026-06-07 21:09:32 +0200

    create unitra error base classes

---
+++ packages/kernel/core/src/utils/error/serializeError.ts
@@ -0,0 +1,19 @@
+import type { SerializedError } from '@unitra/types/utils/error';
+import { UnitraError } from './UnitraError';
+
+export function serializeError ( error: Error ) : SerializedError;
+export function serializeError ( error: unknown ) : unknown;
+
+export function serializeError ( error: unknown ) : SerializedError | unknown {
+  if ( ! ( error instanceof Error ) ) return error;
+
+  return {
+    ...{
+      name: error.name, message: error.message, stack: error.stack,
+      cause: serializeError( ( error as Error & { cause?: unknown } ).cause )
+    },
+    ...( error instanceof UnitraError ? {
+      code: error.code, summary: error.summary, context: error.context
+    } : {} )
+  };
+};
