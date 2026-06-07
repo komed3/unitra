af80ad3c0fcf67a169a0cb3570d126bc89b4fb26
komed3 <pk97@outlook.de>
2026-06-07 22:52:39 +0200

    implement registry (units, prefixes, ...)

---
+++ packages/kernel/core/src/system/registry/Registry.ts
@@ -0,0 +1,46 @@
+import type { AnyRef, IRegistry, RegistryContent, RegistryDef, RegistryEntries } from '@unitra/types/core/registry';
+import type { UnitraContext } from '@unitra/types/core/unitra';
+import { RegistryStorage } from './RegistryStorage';
+
+export class Registry< Ref extends AnyRef > implements IRegistry< Ref > {
+  private readonly items = new RegistryStorage< Ref >();
+
+  constructor ( private readonly ctx: UnitraContext ) {}
+
+  public get size () : number {
+    return this.items.size;
+  }
+
+  public get < R extends Ref > ( ref: R ) : RegistryDef< R > | undefined {
+    return this.items.get( ref );
+  }
+
+  public has ( ref: Ref ) : boolean {
+    return this.items.has( ref );
+  }
+
+  public set < R extends Ref > ( ref: R, def: RegistryDef< R > ) : void {
+    this.items.set( ref, def );
+  }
+
+  public bulk ( input: RegistryContent< Ref > | RegistryEntries< Ref > ) : void {
+    const entries = ( Symbol.iterator in Object( input ) ? input : Object.entries( input ) ) as RegistryEntries< Ref >;
+    for ( const [ ref, def ] of entries ) this.items.set( ref, def );
+  }
+
+  public entries () : IterableIterator< [ Ref, RegistryDef< Ref > ] > {
+    return this.items.entries();
+  }
+
+  public keys () : IterableIterator< Ref > {
+    return this.items.keys();
+  }
+
+  public values () : IterableIterator< RegistryDef< Ref > > {
+    return this.items.values();
+  }
+
+  public filter ( predicate: ( def: RegistryDef< Ref > ) => boolean ) : RegistryDef< Ref >[] {
+    return [ ...this.items.values() ].filter( predicate );
+  }
+}
---
+++ packages/kernel/core/src/system/registry/index.ts
@@ -0,0 +1,26 @@
+import type { IRegistry, RefOf, RegistryContainer, RegistryFactoryMap, RegistryKey } from '@unitra/types/core/registry';
+import type { UnitraContext } from '@unitra/types/core/unitra';
+import type { ConstantRef } from '@unitra/types/def/constant';
+import type { PrefixRef } from '@unitra/types/def/prefix';
+import type { QuantityRef } from '@unitra/types/def/quantity';
+import type { UnitRef } from '@unitra/types/def/unit';
+import { createContainer } from '../../utils/container';
+import { Registry } from './Registry';
+
+export const createRegistryContainer = (
+  ctx: UnitraContext,
+  factories?: Partial< RegistryFactoryMap >
+) : RegistryContainer =>
+  createContainer(
+    ctx,
+    {
+      prefix: ctx => new Registry< PrefixRef >( ctx ),
+      quantity: ctx => new Registry< QuantityRef >( ctx ),
+      unit: ctx => new Registry< UnitRef >( ctx ),
+      constant: ctx => new Registry< ConstantRef >( ctx )
+    },
+    factories
+  );
+
+export const getTypedRegistry = < K extends RegistryKey > ( ctx: UnitraContext, key: K ) =>
+  ctx.registry[ key ]() as unknown as IRegistry< RefOf< K > >;
