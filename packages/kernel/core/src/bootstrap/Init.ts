8a33cadac7c63302d2f9d1d0f9689a6c17ee5121
komed3 <pk97@outlook.de>
2026-06-08 08:28:46 +0200

    freeze ctx

--- packages/kernel/core/src/bootstrap/Init.ts
+++ packages/kernel/core/src/bootstrap/Init.ts
@@ -1,5 +1,21 @@
+import type { UnitraContext } from '@unitra/types/core/unitra';
 import { Logging } from '../utils/logging';
 
 export class Init {
   private static readonly log = Logging.createSource( 'bootstrap' );
+
+  public get VERSION () : number {
+    return 1;
+  }
+
+  private static freezeCtx ( ctx: UnitraContext ) {
+    Init.log.debug( 'freezing context ...' );
+    Object.freeze( ctx );
+
+    Object.freeze( ctx.VERSION );
+    Object.freeze( ctx.hook );
+    Object.freeze( ctx.registry );
+    Object.freeze( ctx.service );
+    Object.freeze( ctx.factory );
+  }
 }
