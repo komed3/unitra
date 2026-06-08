e87b27df7d6573c4157ae91a4a29851e5d92c50c
komed3 <pk97@outlook.de>
2026-06-08 14:06:54 +0200

    create unitra instance

--- packages/kernel/core/src/api/Unitra.ts
+++ packages/kernel/core/src/api/Unitra.ts
@@ -4,4 +4,10 @@ export class Unitra {
   private static readonly log = Logging.createSource( 'unitra' );
   private static cacheRevision: number = -1;
   private static cache: Unitra;
+
+  public static create () : Unitra {
+    return new Unitra();
+  }
+
+  private constructor () {}
 }
--- packages/kernel/core/src/index.ts
+++ packages/kernel/core/src/index.ts
@@ -1,2 +1,4 @@
 import { Unitra } from './api/Unitra';
 export { Unitra };
+
+export const createUnitra = () => Unitra.create();
