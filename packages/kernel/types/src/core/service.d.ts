f5023d5eacaf45f1982356834a28a62307a97ffc
komed3 <pk97@outlook.de>
2026-06-09 16:41:36 +0200

    update asserter interface

--- packages/kernel/types/src/core/service.d.ts
+++ packages/kernel/types/src/core/service.d.ts
@@ -1,6 +1,6 @@
 import type { ErrorCode } from '@unitra/dict/utils';
 import type { CompoundStruct, UnitStruct } from '../def/unit';
-import type { ReferenceState } from '../node';
+import type { Node, ReferenceState, SerializedState } from '../node';
 import type { Container, ContainerFactoryMap } from '../utils/container';
 import type { IUnitraError } from '../utils/error';
 import type { AnyRef, DefOf, InputOf, RefOf, RegistryKey } from './registry';
@@ -8,8 +8,14 @@ import type { AnyRef, DefOf, InputOf, RefOf, RegistryKey } from './registry';
 export interface IAssert {
   isRef < K extends RegistryKey > ( key: K, value: unknown ) : value is RefOf< K >;
   isDef < K extends RegistryKey > ( key: K, value: unknown ) : value is DefOf< K >;
+  isNode ( value: unknown, type?: NodeType ) : value is Node;
+  isState ( value: unknown ) : value is ReferenceState;
+  isSerializedState ( value: unknown ) : value is SerializedState;
   assertRef < K extends RegistryKey > ( key: K, value: unknown ) : asserts value is RefOf< K >;
   assertDef < K extends RegistryKey > ( key: K, value: unknown ) : asserts value is DefOf< K >;
+  assertNode ( value: unknown, type?: NodeType ) : asserts value is Node;
+  assertState ( value: unknown ) : asserts value is ReferenceState;
+  assertSerializedState ( value: unknown ) : asserts value is SerializedState;
 }
 
 export interface IResolve {
