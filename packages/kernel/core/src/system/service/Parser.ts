fd82f8555262f43804d86c5d78f4d1ac3763ae98
komed3 <pk97@outlook.de>
2026-06-09 12:01:08 +0200

    populate cache if not exists

--- packages/kernel/core/src/system/service/Parser.ts
+++ packages/kernel/core/src/system/service/Parser.ts
@@ -1,3 +1,4 @@
+import type { AnyRef } from '@unitra/types/core/registry';
 import type { ParserResult, TokenCache } from '@unitra/types/core/service';
 import type { UnitraContext } from '@unitra/types/core/unitra';
 
@@ -5,6 +6,20 @@ export class Parser {
   private tokenCache: TokenCache | undefined;
 
   constructor ( private readonly ctx: UnitraContext ) {}
+
+  private get token () : TokenCache {
+    return this.tokenCache ??= ( () => {
+      const tokens: TokenCache = new Map();
+
+      for ( const [ key, reg ] of Object.entries( this.ctx.registry ) ) {
+        const map = new Map< string, AnyRef >();
+
+        for ( const item of reg().values() ) {}
+      }
+
+      return tokens;
+    } )();
+  }
 
   public parse ( input: string ) : ParserResult {
     return { state: { nodes: [] } };
