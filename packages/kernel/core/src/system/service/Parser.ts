3779d6d4d6b03c5b87def5f80a3504fce85c3ad9
komed3 <pk97@outlook.de>
2026-06-10 00:34:43 +0200

    Update Parser.ts

--- packages/kernel/core/src/system/service/Parser.ts
+++ packages/kernel/core/src/system/service/Parser.ts
@@ -1,6 +1,7 @@
 import type { AnyRef, RegistryKey } from '@unitra/types/core/registry';
 import type { IParser, ParserResult, TokenCache } from '@unitra/types/core/service';
 import type { UnitraContext } from '@unitra/types/core/unitra';
+import { ParserError } from '../../utils/error';
 import { Logging } from '../../utils/logging';
 
 export class Parser implements IParser {
@@ -39,17 +40,22 @@ export class Parser implements IParser {
     } )();
   }
 
+  private parseInput ( result: ParserResult, input: string ) : void {}
+
   public parse ( input: unknown ) : ParserResult {
     input = this.ctx.hook().run( 'core.service.parser.before', {}, input );
-    const result = {} as ParserResult;
+    const result = { state: { nodes: [] } } as ParserResult;
 
-    if ( this.ctx.service.assert().isState( input ) )
-      result.state = input;
+    if ( this.ctx.service.assert().isState( input ) ) result.state = input;
 
-    else if ( this.ctx.service.assert().isSerializedState( input ) )
-      result.state = this.ctx.service.serialize().toReferenceState( input );
+    else if ( this.ctx.service.assert().isSerializedState( input ) ) {
+      try { result.state = this.ctx.service.serialize().toReferenceState( input ) }
+      catch ( err ) { result.error = new ParserError(
+        'failed to parse serialized state', { context: { input }, cause: err }
+      ) }
+    }
 
-    // ...
+    else this.parseInput( result, String( input ) );
 
     this.ctx.hook().run( 'core.service.parser.after', { result, input } );
     return result;
