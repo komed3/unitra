2953a0650d1bab86db527b389843390600d71fb5
komed3 <pk97@outlook.de>
2026-06-09 11:51:37 +0200

    init parser service

--- packages/kernel/core/src/system/service/Parser.ts
+++ packages/kernel/core/src/system/service/Parser.ts
@@ -1,6 +1,11 @@
-import type { ParserResult } from '@unitra/types/core/service';
+import type { ParserResult, TokenCache } from '@unitra/types/core/service';
+import type { UnitraContext } from '@unitra/types/core/unitra';
 
 export class Parser {
+  private tokenCache: TokenCache | undefined;
+
+  constructor ( private readonly ctx: UnitraContext ) {}
+
   public parse ( input: string ) : ParserResult {
     return { state: { nodes: [] } };
   }
