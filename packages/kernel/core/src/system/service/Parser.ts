1d061fdcd8adb560ec0d0c255170cda31cc36014
komed3 <pk97@outlook.de>
2026-06-09 12:08:07 +0200

    loop over aliases + add logging

--- packages/kernel/core/src/system/service/Parser.ts
+++ packages/kernel/core/src/system/service/Parser.ts
@@ -1,15 +1,20 @@
-import type { AnyRef } from '@unitra/types/core/registry';
+import type { AnyRef, RegistryKey } from '@unitra/types/core/registry';
 import type { ParserResult, TokenCache } from '@unitra/types/core/service';
 import type { UnitraContext } from '@unitra/types/core/unitra';
+import { Logging } from '../../utils/logging';
 
 export class Parser {
+  private static log = Logging.createSource( 'parser' );
   private tokenCache: TokenCache | undefined;
 
   constructor ( private readonly ctx: UnitraContext ) {}
 
   private get token () : TokenCache {
     return this.tokenCache ??= ( () => {
+      Parser.log.debug( 'populate token cache ...' );
+
       const tokens: TokenCache = new Map();
+      let size = 0;
 
       for ( const [ key, reg ] of Object.entries( this.ctx.registry ) ) {
         const map = new Map< string, [ AnyRef, boolean ] >();
@@ -17,9 +22,17 @@ export class Parser {
         for ( const item of reg().values() ) {
           const prefixable = 'prefixable' in item && item.prefixable;
           map.set( item.id, [ item.id, prefixable ] );
+
+          if ( 'aliases' in item && item.aliases?.length )
+            for ( const alias of item.aliases )
+              map.set( alias, [ item.id, prefixable ] );
         }
+
+        tokens.set( key as RegistryKey, map );
+        size += map.size;
       }
 
+      Parser.log.debug( `token cache populated with ${ size } entries` );
       return tokens;
     } )();
   }
