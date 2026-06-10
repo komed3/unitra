22463630cefb2091a54d0e23b7e987f37beab51d
komed3 <pk97@outlook.de>
2026-06-10 11:56:43 +0200

    fix typo

--- packages/kernel/types/src/core/parser.d.ts
+++ packages/kernel/types/src/core/parser.d.ts
@@ -15,15 +15,15 @@ export type ParserGrammar = Map< RegistryKey, ParserGrammarMap >;
 export type ParserIDToken = { type: 'identifier', value: string };
 export type ParserOPToken = { type: 'operator', value: '*' | '/' | '^' };
 export type ParserNumberToken = { type: 'number', value: number };
-export type ParserLParenToken = { type: 'lparan' };
-export type ParserRParanToken = { type: 'rparan' };
+export type ParserLParenToken = { type: 'lparen' };
+export type ParserRParenToken = { type: 'rparen' };
 
 export type ParserToken =
   | ParserIDToken
   | ParserOPToken
   | ParserNumberToken
   | ParserLParenToken
-  | ParserRParanToken;
+  | ParserRParenToken;
 
 export type ParserResult = {
   state: ReferenceState;
