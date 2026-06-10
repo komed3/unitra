00aa1a8f2a4f7435ac542b8ee574dc10a2dda656
komed3 <pk97@outlook.de>
2026-06-10 10:02:18 +0200

    get grammar

--- packages/kernel/core/src/system/parser/Grammar.ts
+++ packages/kernel/core/src/system/parser/Grammar.ts
@@ -1,5 +1,14 @@
+import type { ParserGrammar } from '@unitra/types/core/parser';
 import type { UnitraContext } from '@unitra/types/core/unitra';
 
 export class Grammar {
+  private cache: ParserGrammar | undefined;
+
   constructor ( private readonly ctx: UnitraContext ) {}
+
+  private populateGrammarCache () : ParserGrammar {}
+
+  public get grammar () : ParserGrammar {
+    return this.cache ??= this.populateGrammarCache();
+  }
 }
--- packages/kernel/types/src/core/parser.d.ts
+++ packages/kernel/types/src/core/parser.d.ts
@@ -1,6 +1,9 @@
 import type { ErrorCode } from '@unitra/dict/utils';
 import type { ReferenceState } from '../node';
 import type { IUnitraError } from '../utils/error';
+import type { AnyRef, RegistryKey } from './registry';
+
+export type ParserGrammar = Map< RegistryKey, Map< string, [ AnyRef, boolean ] > >;
 
 export type ParserResult = {
   state: ReferenceState;
