db8dd703827736e5f798fc467ce0c744e0a54783
komed3 <pk97@outlook.de>
2026-06-10 09:31:02 +0200

    input will be part of result

--- packages/kernel/types/src/core/parser.d.ts
+++ packages/kernel/types/src/core/parser.d.ts
@@ -4,6 +4,7 @@ import type { IUnitraError } from '../utils/error';
 
 export type ParserResult = {
   state: ReferenceState;
+  input: unknown;
   error?: IUnitraError< ErrorCode.PARSER_ERROR >;
 };
 
