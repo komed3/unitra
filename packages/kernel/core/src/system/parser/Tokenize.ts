2ed91c7467b29b3f995322999a293247927bbce4
komed3 <pk97@outlook.de>
2026-06-10 12:11:27 +0200

    prep. tokenizer maps

--- packages/kernel/core/src/system/parser/Tokenize.ts
+++ packages/kernel/core/src/system/parser/Tokenize.ts
@@ -4,5 +4,23 @@ import { Logging } from '../../utils/logging';
 export class Tokenize {
   private static readonly log = Logging.createSource( 'parser::tokenize' );
 
+  private static readonly SUPER_MAP = {
+    '⁰': '0', '¹': '1', '²': '2', '³': '3', '⁴': '4', '⁵': '5',
+    '⁶': '6', '⁷': '7', '⁸': '8', '⁹': '9', '⁻': '-', '⁺': '+'
+  } as const;
+
+  private static readonly OPERATOR_MAP = {
+    '*': '*', '×': '*', '·': '*', '/': '/', '^': '^'
+  } as const;
+
+  private static readonly NATURAL_MAP = {
+    'per': '/', 'over': '/', 'square': '^2', 'squared': '^2',
+    'cube': '^3', 'cubed': '^3', 'reciprocal': '^-1'
+  } as const;
+
+  private static readonly NUMBER_MOD = [
+    '.', 'e', '+', '-'
+  ] as const;
+
   constructor ( private readonly ctx: UnitraContext ) {}
 }
