4f6d0a62de495845076ed59da2e323119cb3a2aa
komed3 <pk97@outlook.de>
2026-06-10 09:50:01 +0200

    add parser hooks + pass given state direct to result

--- packages/kernel/core/src/system/parser/Parser.ts
+++ packages/kernel/core/src/system/parser/Parser.ts
@@ -1,11 +1,18 @@
 import type { IParser, ParserResult } from '@unitra/types/core/parser';
 import type { UnitraContext } from '@unitra/types/core/unitra';
+import { Logging } from '../../utils/logging';
 
 export class Parser implements IParser {
+  private static log = Logging.createSource( 'parser' );
   constructor ( private readonly ctx: UnitraContext ) {}
 
   public parse ( input: unknown ) : ParserResult {
+    input = this.ctx.hook().run( 'core.parser.input', {}, input );
     const result: ParserResult = { input, state: { nodes: [] } };
+
+    if ( this.ctx.service.assert().isState( input ) ) result.state = input;
+
+    this.ctx.hook().run( 'core.parser.result', { result } );
     return result;
   }
 }
