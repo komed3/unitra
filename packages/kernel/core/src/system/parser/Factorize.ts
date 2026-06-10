84d074b93b4b1867bd0a4b22e660ec1741c9e2a3
komed3 <pk97@outlook.de>
2026-06-10 23:24:34 +0200

    Update Factorize.ts

--- packages/kernel/core/src/system/parser/Factorize.ts
+++ packages/kernel/core/src/system/parser/Factorize.ts
@@ -1,3 +1,4 @@
+import type { ParsedExpression, ParserToken } from '@unitra/types/core/parser';
 import type { UnitraContext } from '@unitra/types/core/unitra';
 import { Logging } from '../../utils/logging';
 
@@ -5,4 +6,10 @@ export class Factorize {
   private static readonly log = Logging.createSource( 'parser::factorize' );
 
   constructor ( private readonly ctx: UnitraContext ) {}
+
+  public run ( tokens: ParserToken[] ) : ParsedExpression {
+    const result: ParsedExpression = { factors: [] };
+
+    return result;
+  }
 }
