1a5fad9f45c980f5901fd5142fd9fa8ea2949903
komed3 <pk97@outlook.de>
2026-06-12 01:16:46 +0200

    Create NodeBuilder.ts

---
+++ packages/kernel/core/src/system/parser/NodeBuilder.ts
@@ -0,0 +1,31 @@
+import type { ParsedFactor } from '@unitra/types/core/parser';
+import type { ConstantRef } from '@unitra/types/def/constant';
+import type { PrefixRef } from '@unitra/types/def/prefix';
+import type { UnitRef } from '@unitra/types/def/unit';
+import type { Node } from '@unitra/types/node';
+import { ParserError } from '../../utils/error';
+
+export class FactorNodeBuilder {
+  public static toNode ( term: ParsedFactor ) : Node {
+    const { token, exp } = term;
+
+    if ( token.type === 'number' ) return {
+      type: 'factor', value: token.value, exp
+    };
+
+    if ( token.type !== 'compound' ) throw new ParserError(
+      `unresolved identifier "${ token.value }"`,
+      { context: { tokens: [ token ] } }
+    );
+
+    const [ { key, ref }, prefix ] = token.value.reverse();
+    if ( key === 'constant' ) return {
+      type: 'constant', constant: ref as ConstantRef, exp
+    };
+
+    else return {
+      type: 'unit', unit: ref as UnitRef, exp,
+      prefix: prefix ? prefix.ref as PrefixRef : undefined
+    };
+  }
+}
