a2392f029e2abf23c968b752448c1bf430df53c2
komed3 <pk97@outlook.de>
2026-06-11 14:30:20 +0200

    resolve direct units + constants

--- packages/kernel/core/src/system/parser/Resolve.ts
+++ packages/kernel/core/src/system/parser/Resolve.ts
@@ -1,4 +1,4 @@
-import type { ParserToken, ResolvedToken } from '@unitra/types/core/parser';
+import type { ParserCompoundToken, ParserToken, ResolvedToken } from '@unitra/types/core/parser';
 import type { UnitraContext } from '@unitra/types/core/unitra';
 import { Logging } from '../../utils/logging';
 import type { Grammar } from './Grammar';
@@ -10,6 +10,16 @@ export class Resolve {
     private readonly ctx: UnitraContext,
     private readonly grammar: Grammar
   ) {}
+
+  private resolveIdentifier ( value: string, tokens: ParserToken[], index: number ) : ParserCompoundToken {
+    Resolve.log.debug( `resolving identifier "${ value }" ...` );
+
+    const u = this.grammar.find( 'unit', value );
+    if ( u ) return { type: 'compound', value: [ u ] };
+
+    const c = this.grammar.find( 'constant', value );
+    if ( c ) return { type: 'compound', value: [ c ] };
+  }
 
   public run ( tokens: ParserToken[] ) : ResolvedToken[] {}
 }
