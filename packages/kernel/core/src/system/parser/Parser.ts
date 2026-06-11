6a129fe1270182e13467df14233389364970d35a
komed3 <pk97@outlook.de>
2026-06-11 02:21:16 +0200

    connect (grammar) resolver class

--- packages/kernel/core/src/system/parser/Parser.ts
+++ packages/kernel/core/src/system/parser/Parser.ts
@@ -4,19 +4,24 @@ import { ParserError } from '../../utils/error';
 import { Logging } from '../../utils/logging';
 import { Factorize } from './Factorize';
 import { Grammar } from './Grammar';
+import { Resolve } from './Resolve';
 import { Tokenize } from './Tokenize';
 
 export class Parser implements IParser {
   private static readonly log = Logging.createSource( 'parser' );
 
-  private readonly factorize: Factorize;
   private readonly grammar: Grammar;
+
   private readonly tokenize: Tokenize;
+  private readonly resolve: Resolve;
+  private readonly factorize: Factorize;
 
   constructor ( private readonly ctx: UnitraContext ) {
-    this.factorize = new Factorize( ctx );
     this.grammar = new Grammar( ctx );
+
     this.tokenize = new Tokenize( ctx );
+    this.resolve = new Resolve( ctx );
+    this.factorize = new Factorize( ctx );
   }
 
   private parseInput ( result: ParserResult, input: string ) : void {
