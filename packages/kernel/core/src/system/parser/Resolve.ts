9daf0d3adfa600b462b43674da92d0d5603eac3c
komed3 <pk97@outlook.de>
2026-06-11 14:00:21 +0200

    prep. resolve run method

--- packages/kernel/core/src/system/parser/Resolve.ts
+++ packages/kernel/core/src/system/parser/Resolve.ts
@@ -1,3 +1,4 @@
+import type { ParserToken, ResolvedToken } from '@unitra/types/core/parser';
 import type { UnitraContext } from '@unitra/types/core/unitra';
 import { Logging } from '../../utils/logging';
 import type { Grammar } from './Grammar';
@@ -9,4 +10,6 @@ export class Resolve {
     private readonly ctx: UnitraContext,
     private readonly grammar: Grammar
   ) {}
+
+  public run ( tokens: ParserToken[] ) : ResolvedToken[] {}
 }
