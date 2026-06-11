c83873f1cb3412d8532a208952a82cfb34ccbe06
komed3 <pk97@outlook.de>
2026-06-12 01:25:58 +0200

    Update Parser.ts

--- packages/kernel/core/src/system/parser/Parser.ts
+++ packages/kernel/core/src/system/parser/Parser.ts
@@ -1,13 +1,10 @@
 import type { IParser, ParsedFactor, ParserResult } from '@unitra/types/core/parser';
 import type { UnitraContext } from '@unitra/types/core/unitra';
-import type { ConstantRef } from '@unitra/types/def/constant';
-import type { PrefixRef } from '@unitra/types/def/prefix';
-import type { UnitRef } from '@unitra/types/def/unit';
-import type { Node } from '@unitra/types/node';
 import { ParserError } from '../../utils/error';
 import { Logging } from '../../utils/logging';
 import { Factorize } from './Factorize';
 import { Grammar } from './Grammar';
+import { NodeBuilder } from './NodeBuilder';
 import { Resolve } from './Resolve';
 import { Tokenize } from './Tokenize';
 
@@ -28,53 +25,26 @@ export class Parser implements IParser {
     this.factorize = new Factorize( ctx );
   }
 
-  private getNode ( term: ParsedFactor ) : Node {
-    if ( term.token.type === 'number' ) return {
-      type: 'factor', value: term.token.value, exp: term.exp
-    };
-
-    else if ( term.token.type === 'compound' ) {
-      const [ ref, prefix ] = term.token.value.reverse();
-
-      if ( ref.key === 'constant' ) return {
-        type: 'constant', constant: ref.ref as ConstantRef, exp: term.exp
-      };
-
-      else return {
-        type: 'unit', unit: ref.ref as UnitRef, exp: term.exp,
-        prefix: prefix ? prefix.ref as PrefixRef : undefined
-      };
-    }
-
-    throw new ParserError(
-      `unresolved identifier "${ term.token.value }"`,
-      { context: {} }
-    );
+  private pipeline ( input: string ) : ParsedFactor[] {
+    const raw = this.tokenize.run( input );
+    const resolved = this.resolve.run( raw );
+    return this.factorize.run( resolved );
   }
 
-  private getNodes ( factors: ParsedFactor[] ) : Node[] {
-    const nodes: Node[] = [];
-    for ( const term of factors ) nodes.push( this.getNode( term ) );
-    return nodes;
-  }
-
-  private parseInput ( result: ParserResult, input: string ) : void {
+  private parseString ( result: ParserResult, input: string ) : void {
     try {
       Parser.log.debug( `parsing "${ input }" ...` );
+      const factors = this.pipeline( input );
 
-      const rawTokens = this.tokenize.run( input );
-      const resolvedTokens = this.resolve.run( rawTokens );
-      const factors = this.factorize.run( resolvedTokens );
-
-      result.state = { nodes: this.getNodes( factors ) };
+      Parser.log.debug( `building nodes ...` );
+      result.state.nodes = NodeBuilder.toNodes( factors );
     }
 
     catch ( err ) {
       Parser.log.error( `failed to parse input "${ input }"` );
-
-      if ( err instanceof ParserError ) result.error = err;
-      else result.error = new ParserError(
-        'failed to parse input', { context: { input }, cause: err }
+      result.error = err instanceof ParserError ? err : new ParserError(
+        `failed to parse input "${ input }"`,
+        { context: { input }, cause: err }
       );
     }
   }
@@ -87,12 +57,13 @@ export class Parser implements IParser {
 
     else if ( this.ctx.service.assert().isSerializedState( input ) ) {
       try { result.state = this.ctx.service.serialize().toReferenceState( input ) }
-      catch ( err ) { result.error = new ParserError( 'failed to parse serialized state',
+      catch ( err ) { result.error = new ParserError(
+        'failed to parse serialized state',
         { context: { input }, cause: err }
       ) }
     }
 
-    else this.parseInput( result, String( input ) );
+    else this.parseString( result, String( input ) );
 
     this.ctx.hook().run( 'core.parser.result', { result } );
     return result;
