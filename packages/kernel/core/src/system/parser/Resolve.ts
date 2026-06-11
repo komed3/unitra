cd06896d64d894cc06855d73fc4d77a0bb7466ea
komed3 <pk97@outlook.de>
2026-06-11 02:24:38 +0200

    Update Resolve.ts

--- packages/kernel/core/src/system/parser/Resolve.ts
+++ packages/kernel/core/src/system/parser/Resolve.ts
@@ -1,7 +1,10 @@
 import type { UnitraContext } from '@unitra/types/core/unitra';
+import { Logging } from '../../utils/logging';
 import type { Grammar } from './Grammar';
 
 export class Resolve {
+  private static readonly log = Logging.createSource( 'parser::resolve' );
+
   constructor (
     private readonly ctx: UnitraContext,
     private readonly grammar: Grammar
