cb789ef0ceaba01922290d59c946394350d0718f
komed3 <pk97@outlook.de>
2026-06-12 16:48:56 +0200

    create formatter classes

---
+++ packages/kernel/core/src/system/formatter/PlainFormatter.ts
@@ -0,0 +1,4 @@
+import type { IFormatter } from '@unitra/types/core/formatter';
+import { Formatter } from './Formatter';
+
+export class PlainFormatter extends Formatter implements IFormatter {}
---
+++ packages/kernel/core/src/system/formatter/TextFormatter.ts
@@ -0,0 +1,4 @@
+import type { IFormatter } from '@unitra/types/core/formatter';
+import { Formatter } from './Formatter';
+
+export class TextFormatter extends Formatter implements IFormatter {}
