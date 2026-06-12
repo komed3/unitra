8d28f7986d0928423e0e632af82e2d9b5aed9e65
komed3 <pk97@outlook.de>
2026-06-12 17:33:11 +0200

    protected def getter

--- packages/kernel/core/src/system/formatter/Formatter.ts
+++ packages/kernel/core/src/system/formatter/Formatter.ts
@@ -1,6 +1,12 @@
 import type { IFormatter } from '@unitra/types/core/formatter';
+import type { RefOf, RegistryDef, RegistryKey } from '@unitra/types/core/registry';
 import type { UnitraContext } from '@unitra/types/core/unitra';
+import { getTypedRegistry } from '../registry';
 
 export abstract class Formatter implements IFormatter {
   constructor ( protected readonly ctx: UnitraContext ) {}
+
+  protected get < K extends RegistryKey > ( key: K, ref: RefOf< K > ) : RegistryDef< RefOf< K > > | undefined {
+    return getTypedRegistry( this.ctx, key ).get( ref );
+  }
 }
