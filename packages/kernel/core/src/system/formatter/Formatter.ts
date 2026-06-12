4e2519cbc6c368a523b94ca6a2c22453d69a8164
komed3 <pk97@outlook.de>
2026-06-12 17:36:30 +0200

    pick language

--- packages/kernel/core/src/system/formatter/Formatter.ts
+++ packages/kernel/core/src/system/formatter/Formatter.ts
@@ -1,5 +1,5 @@
-import { Format } from '@unitra/dict/common';
-import type { FormatGroup } from '@unitra/types/common';
+import { Format, Lang } from '@unitra/dict/common';
+import type { FormatGroup, LangGroup } from '@unitra/types/common';
 import type { IFormatter } from '@unitra/types/core/formatter';
 import type { RefOf, RegistryDef, RegistryKey } from '@unitra/types/core/registry';
 import type { UnitraContext } from '@unitra/types/core/unitra';
@@ -12,7 +12,11 @@ export abstract class Formatter implements IFormatter {
     return getTypedRegistry( this.ctx, key ).get( ref );
   }
 
-  protected pickFormat< T > ( group: FormatGroup< T >, format: Format ) : T {
+  protected pickFormat < T > ( group: FormatGroup< T >, format: Format ) : T {
     return ( group[ format ] ?? group[ Format.PLAIN ] );
+  }
+
+  protected pickLang < T > ( group: LangGroup< T >, lang: Lang ) : T {
+    return ( group[ lang ] ?? group[ Lang.EN ] ?? Object.values( group )[ 0 ] );
   }
 }
