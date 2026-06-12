958ed2d18726047b6aafe4feecb5670b131b436c
komed3 <pk97@outlook.de>
2026-06-12 17:15:00 +0200

    ctx needs to be protected

--- packages/kernel/core/src/system/formatter/Formatter.ts
+++ packages/kernel/core/src/system/formatter/Formatter.ts
@@ -2,5 +2,5 @@ import type { IFormatter } from '@unitra/types/core/formatter';
 import type { UnitraContext } from '@unitra/types/core/unitra';
 
 export abstract class Formatter implements IFormatter {
-  constructor ( private readonly ctx: UnitraContext ) {}
+  constructor ( protected readonly ctx: UnitraContext ) {}
 }
