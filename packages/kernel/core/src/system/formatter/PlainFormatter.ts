670eca750763d6b0549fa8251870a0aea0faca0f
komed3 <pk97@outlook.de>
2026-06-12 18:28:32 +0200

    plain formatter will consume formatter options

--- packages/kernel/core/src/system/formatter/PlainFormatter.ts
+++ packages/kernel/core/src/system/formatter/PlainFormatter.ts
@@ -1,15 +1,9 @@
-import type { Lang, System } from '@unitra/dict/common';
-import type { IFormatter } from '@unitra/types/core/formatter';
+import type { FormatterOptions, IFormatter } from '@unitra/types/core/formatter';
 import type { ReferenceState } from '@unitra/types/node';
 import { Formatter } from './Formatter';
 
-export type PlainFormatterOptions = {
-  system?: System;
-  lang?: Lang;
-};
-
 export class PlainFormatter extends Formatter implements IFormatter {
-  public format ( state: ReferenceState, options?: PlainFormatterOptions ) : string {
+  public format ( state: ReferenceState, options?: FormatterOptions ) : string {
     return this.ctx.hook().run( 'core.formatter.format', { state, options }, '' );
   }
 }
