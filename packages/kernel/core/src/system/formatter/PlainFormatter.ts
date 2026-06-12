c8a2168eb2338dc49894377bf7e500977faff38e
komed3 <pk97@outlook.de>
2026-06-12 23:28:07 +0200

    Update PlainFormatter.ts

--- packages/kernel/core/src/system/formatter/PlainFormatter.ts
+++ packages/kernel/core/src/system/formatter/PlainFormatter.ts
@@ -1,8 +1,11 @@
+import { Format } from '@unitra/dict/common';
 import type { FormatterOptions, IFormatter } from '@unitra/types/core/formatter';
 import type { ReferenceState } from '@unitra/types/node';
 import { Formatter } from './Formatter';
 
 export class PlainFormatter extends Formatter implements IFormatter {
+  protected override readonly _format = Format.PLAIN;
+
   public format ( state: ReferenceState, options?: FormatterOptions ) : string {
     return this.ctx.hook().run( 'core.formatter.format', { state, options }, '' );
   }
