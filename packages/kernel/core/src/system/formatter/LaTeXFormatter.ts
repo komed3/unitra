37ffd558c0a1e8332989e68f8845bb678d1cc39d
komed3 <pk97@outlook.de>
2026-06-13 01:20:12 +0200

    rename latex formatter

--- packages/kernel/core/src/system/formatter/LaTeXFormatter.ts
+++ packages/kernel/core/src/system/formatter/LaTeXFormatter.ts
@@ -3,7 +3,7 @@ import type { IFormatter } from '@unitra/types/core/formatter';
 import type { ReferenceState } from '@unitra/types/node';
 import { Formatter } from './Formatter';
 
-export class LatexFormatter extends Formatter implements IFormatter {
+export class LaTeXFormatter extends Formatter implements IFormatter {
   protected override readonly format = Format.LATEX;
 
   public out ( state: ReferenceState ) : string {
