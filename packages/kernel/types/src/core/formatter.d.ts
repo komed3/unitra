967a3c3281df95ed0f8fc2bbe80260b1736db89e
komed3 <pk97@outlook.de>
2026-06-12 18:27:41 +0200

    declare formatter options

--- packages/kernel/types/src/core/formatter.d.ts
+++ packages/kernel/types/src/core/formatter.d.ts
@@ -1,5 +1,33 @@
+import type { Lang, System } from '@unitra/dict/common';
 import type { ReferenceState } from '../node';
+import type { RefOf, RegistryKey } from './registry';
+
+export type SymbolIDFilter = {
+  [ K in RegistryKey ]?: {
+    [ R in RefOf< K > ]?: string;
+  };
+};
+
+export type SymbolOptions = {
+  system?: System;
+  lang?: Lang;
+  id?: string;
+};
+
+export type FormatterOptions = {
+  fraction?: boolean;
+  deprecated?: 'warn' | 'throw' | 'ignore';
+  numeric?: {
+    format?: 'decimal' | 'scientific' | 'engineering' | 'compact';
+    precision?: number;
+  };
+  filter?: {
+    system?: System;
+    lang?: Lang;
+    symbols?: SymbolIDFilter;
+  };
+};
 
 export interface IFormatter {
-  format ( state: ReferenceState, options?: object ) : string;
+  format ( state: ReferenceState, options?: FormatterOptions ) : string;
 }
