643413699bb6b7992a1c93c95d45029c3bfa1f2b
komed3 <pk97@outlook.de>
2026-06-12 17:50:54 +0200

    pick symbol (filtered by system + language)

--- packages/kernel/core/src/system/formatter/Formatter.ts
+++ packages/kernel/core/src/system/formatter/Formatter.ts
@@ -1,5 +1,5 @@
-import { Format, Lang } from '@unitra/dict/common';
-import type { FormatGroup, LangGroup } from '@unitra/types/common';
+import { Format, Lang, type System } from '@unitra/dict/common';
+import type { FormatGroup, LangGroup, Name, Symbol } from '@unitra/types/common';
 import type { IFormatter } from '@unitra/types/core/formatter';
 import type { RefOf, RegistryDef, RegistryKey } from '@unitra/types/core/registry';
 import type { UnitraContext } from '@unitra/types/core/unitra';
@@ -13,14 +13,26 @@ export abstract class Formatter implements IFormatter {
   }
 
   protected pickFormat < T > ( group: FormatGroup< T >, format: Format ) : T {
-    return ( group[ format ] ?? group[ Format.PLAIN ] );
+    return group[ format ] ?? group[ Format.PLAIN ];
   }
 
-  protected pickLang < T > ( group: LangGroup< T >, lang: Lang ) : T {
-    return ( group[ lang ] ?? group[ Lang.EN ] ?? Object.values( group )[ 0 ] );
+  protected pickLang < T > ( group: LangGroup< T > | undefined, lang: Lang ) : T | undefined {
+    return group ? group[ lang ] ?? group[ Lang.EN ] ?? Object.values( group )[ 0 ] : undefined;
   }
 
-  protected plural ( text: [ string, string? ] | string, count: number ) : string {
+  protected plural ( text: Name, count: number ) : string {
     return typeof text === 'string' ? text : count === 1 ? text[ 0 ] : text[ 1 ] || text[ 0 ];
+  }
+
+  protected pickSymbol ( symbols: readonly Symbol[], opt: { system?: System, lang?: Lang } = {} ) : Symbol | undefined {
+    if ( ! symbols.length ) return undefined;
+
+    const filtered = symbols.filter( ( { context } ) => 
+      ( ! context?.system || ( opt.system ? context.system.includes( opt.system ) : true ) ) &&
+      ( ! context?.lang || ( opt.lang ? context.lang === opt.lang : true ) )
+    );
+
+    return filtered.find( s => s.canonical ) ?? filtered[ 0 ] ??
+      symbols.find( s => s.canonical ) ?? symbols[ 0 ];
   }
 }
