1ae5e5eb977ced774b89e71e80bd10bd1c6f382c
komed3 <pk97@outlook.de>
2026-06-12 23:03:52 +0200

    generic node resolver

--- packages/kernel/core/src/system/formatter/Formatter.ts
+++ packages/kernel/core/src/system/formatter/Formatter.ts
@@ -1,9 +1,17 @@
 import type { IFormatter } from '@unitra/types/core/formatter';
 import type { UnitraContext } from '@unitra/types/core/unitra';
-import type { ReferenceState } from '@unitra/types/node';
+import type { Node, ReferenceState } from '@unitra/types/node';
 
 export abstract class Formatter implements IFormatter {
   constructor ( protected readonly ctx: UnitraContext ) {}
+
+  protected resolveNode ( node: Node ) {
+    const res = {};
+
+    for ( const [ k, val ] of Object.entries( node ) ) {
+      if ( k in this.ctx.registry ) res[ k ] = this.resolveSymbol( k, val );
+    }
+  }
 
   public abstract format ( state: ReferenceState ) : string;
 }
