1addcd6b0f9172df18c2da22bdb175555a3f7e6a
komed3 <pk97@outlook.de>
2026-06-13 14:55:37 +0200

    prep. latex formatter

--- packages/kernel/core/src/system/formatter/LaTeXFormatter.ts
+++ packages/kernel/core/src/system/formatter/LaTeXFormatter.ts
@@ -1,7 +1,24 @@
 import { Format } from '@unitra/dict/common';
-import type { IFormatter } from '@unitra/types/core/formatter';
+import type { FormatterOptions, FormatterRenderer, IFormatter, NumberPartRenderer } from '@unitra/types/core/formatter';
 import { Formatter } from './Formatter';
 
 export class LaTeXFormatter extends Formatter implements IFormatter {
   protected override readonly format = Format.LATEX;
+
+  protected override get defaults () : FormatterOptions {
+    return { ...super.defaults,
+      numeric: { ...super.defaults.numeric,
+        notation: 'scientific',
+        scientificStyle: 'power'
+      }
+    };
+  }
+
+  protected override get numberRenderer () : NumberPartRenderer {
+    return { ...super.numberRenderer };
+  }
+
+  protected override get renderer () : FormatterRenderer {
+    return { ...super.renderer };
+  }
 }
