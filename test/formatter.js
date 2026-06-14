5a9632b3543b791ef04bdedd60c9e7f7ad5bfcfc
komed3 <pk97@outlook.de>
2026-06-14 15:05:25 +0200

    run the formatter tests

--- test/formatter.js
+++ test/formatter.js
@@ -1,8 +1,6 @@
 import { addPlugins } from '../packages/kernel/core/dist/bootstrap/index.js';
 import { unitra } from '../packages/kernel/core/dist/index.js';
-import { Logging } from '../packages/kernel/core/dist/utils/logging/index.js';
 import { Lang } from '../packages/kernel/dict/dist/common.js';
-import { LogLevel } from '../packages/kernel/dict/dist/utils.js';
 import siPrefixPlugin from '../packages/plugin/prefix/si/dist/plugin.js';
 import baseQuantityPlugin from '../packages/plugin/quantity/base/dist/plugin.js';
 import siBaseUnitPlugin from '../packages/plugin/unit/si/base/dist/plugin.js';
@@ -21,10 +19,27 @@ const value = -325940000;
 const state = U.parse( unit ).state;
 
 const test = ( as ) => {
+  console.log( as.toUpperCase() + ':' );
   console.log( f( as, state, {}, value ) );
   console.log( f( as, state, { fraction: true, numeric: { sign: 'never', precision: 2 } }, value ) );
   console.log( f( as, state, { numeric: { notation: 'scientific', scientificStyle: 'power' } }, value ) );
   console.log( f( as, state, { fraction: true, numeric: { notation: 'scientific', scientificStyle: 'power' } }, value ) );
   console.log( f( as, state, { lang: Lang.DE, fraction: true, numeric: { notation: 'compact' } }, value ) );
-  console.log( f( as, state, { lang: Lang.DE, numeric: { precision: 3 } }, value ) );
+  console.log( f( as, state, { lang: Lang.DE, numeric: { notation: 'scientific', precision: 1 } }, value ) );
 };
+
+// PLAIN FORMATTER
+
+test( 'plain' );
+
+// UNICODE FORMATTER
+
+test( 'unicode' );
+
+// LATEX FORMATTER
+
+test( 'latex' );
+
+// TEXT FORMATTER
+
+test( 'text' );
