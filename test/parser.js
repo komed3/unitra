7039fc4295250c6672e98dbb407ccfb71679774a
komed3 <pk97@outlook.de>
2026-06-14 14:45:48 +0200

    implement parser test unit

---
+++ test/parser.js
@@ -0,0 +1,33 @@
+import { addPlugins } from '../packages/kernel/core/dist/bootstrap/index.js';
+import { unitra } from '../packages/kernel/core/dist/index.js';
+import siPrefixPlugin from '../packages/plugin/prefix/si/dist/plugin.js';
+import baseQuantityPlugin from '../packages/plugin/quantity/base/dist/plugin.js';
+import siBaseUnitPlugin from '../packages/plugin/unit/si/base/dist/plugin.js';
+
+addPlugins(
+  siBaseUnitPlugin,
+  siPrefixPlugin,
+  baseQuantityPlugin
+);
+
+const U = unitra();
+const s = U.serialize.bind( U );
+const p = U.parse.bind( U );
+
+const test = ( input ) => console.log( s( p( input ).state ) );
+
+// BASIC TESTS
+
+test( '(m/s)^-2' );
+test( '((m/s)^2)^3' );
+test( '(((m)))' );
+test( '50*(m/s)^2' );
+test( '(m/s^-2)^-3' );
+test( '(((kilo-meter^3*(s*s/m))/daK^-2*candela)*50)' );
+test( '((s*kg)/(m^2)*cd^-1)*20s' );
+test( '(((m^2)^3/s^-4)^2)^-1' );
+
+// SPECIAL FORMATS
+
+test( { nodes: [ { type: 'unit', unit: 'cd', exp: 1 } ] } );
+test( '$1::k:m^1*s^-1' );
