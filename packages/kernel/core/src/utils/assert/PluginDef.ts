2fcbab3f20e1387503ca309cadb0a8599125fa7a
komed3 <pk97@outlook.de>
2026-06-15 19:06:16 +0200

    min fix

--- packages/kernel/core/src/utils/assert/PluginDef.ts
+++ packages/kernel/core/src/utils/assert/PluginDef.ts
@@ -1,12 +1,12 @@
 import type { PluginDefinition } from '@unitra/types/core/plugin';
-import { Semver } from '../Semver';
 import { UnitraError } from '../error';
+import { AssertSemver } from './Semver';
 
 export class AssertPluginDef {
   public static is ( value: unknown ) : value is PluginDefinition {
     return typeof value === 'object' && value !== null &&
       'id' in value && typeof value.id === 'string' &&
-      'version' in value && typeof Semver.isSemver( value.version ) &&
+      'version' in value && typeof AssertSemver.is( value.version ) &&
       'meta' in value && typeof value.meta === 'object' && value.meta !== null &&
       'name' in value.meta && typeof value.meta.name === 'string';
   }
