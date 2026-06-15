4f91df611829217f2a4467a4bf6ccda0f7444cbf
komed3 <pk97@outlook.de>
2026-06-15 18:57:26 +0200

    make plugin def asserter a static class

---
+++ packages/kernel/core/src/utils/assert/PluginDef.ts
@@ -0,0 +1,20 @@
+import type { PluginDefinition } from '@unitra/types/core/plugin';
+import { Semver } from '../Semver';
+import { UnitraError } from '../error';
+
+export class AssertPluginDef {
+  public static is ( value: unknown ) : value is PluginDefinition {
+    return typeof value === 'object' && value !== null &&
+      'id' in value && typeof value.id === 'string' &&
+      'version' in value && typeof Semver.isSemver( value.version ) &&
+      'meta' in value && typeof value.meta === 'object' && value.meta !== null &&
+      'name' in value.meta && typeof value.meta.name === 'string';
+  }
+
+  public static assert ( value: unknown ) : asserts value is PluginDefinition {
+    if ( ! this.is( value ) ) throw UnitraError.from(
+      new TypeError( `expected a valid plugin definition` ),
+      { context: { value } }
+    );
+  }
+}
